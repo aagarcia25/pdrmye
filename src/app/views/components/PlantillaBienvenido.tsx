f9ac46c6df99b18e2794f17eec5693e0b761b057
gcantre <gcantu@cecapmex.com>
2022-09-08 11:28:59 -0500

    Perfil diseñado y eliminando líneas que no se usan

--- src/app/views/components/PlantillaBienvenido.tsx
+++ src/app/views/components/PlantillaBienvenido.tsx
@@ -1,15 +1,4 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Tooltip from "@mui/material/Tooltip";
-import IconButton from "@mui/material/IconButton";
-import SearchIcon from "@mui/icons-material/Search";
-import RefreshIcon from "@mui/icons-material/Refresh";
-import Inicio from "../components/Inicio";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { COLOR } from "../../styles/colors";
--- src/app/views/components/perfil/Perfil.tsx
+++ src/app/views/components/perfil/Perfil.tsx
@@ -1,16 +1,214 @@
-import { Box } from "@mui/material";
+import { Box, Divider, Fab, Input, TextField, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { COLOR } from "../../../styles/colors";
+import PersonIcon from "@mui/icons-material/Person";
+import SaveIcon from "@mui/icons-material/Save";
+import SearchIcon from "@mui/icons-material/Search";
+import UploadIcon from "@mui/icons-material/Upload";
 
-export function Perfil(){
+export function Perfil() {
 
-    return (
-    <Box sx={{
-        backgroundColor: COLOR.doradoNL,
-        width: '100vw',
-        height: '100vh',
-    }}>
-        
+    const tamanioTextField ="70%";
+
+  return (
+    <Box
+      sx={{
+        //Fondo
+        backgroundColor:  COLOR.blanco,
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <Box
+        sx={{
+          //Espacio para el Título
+          width: "7%",
+          height: "6%",
+          backgroundColor: COLOR.doradoNL,
+        }}
+      >
+        <Typography
+          sx={{
+            //Perfil
+            color: COLOR.blanco,
+          }}
+          variant="h3"
+        >
+          Perfil
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          //Fondo contenido
+          backgroundColor:  COLOR.blanco,
+          width: "100%",
+          height: "85%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            // Datos de Usuario
+            backgroundColor: COLOR.doradoNL,
+            width: "30%",
+            height: "90%",
+            borderColor: 'primary.main' ,
+            border:1,
+            borderBottom:0,
+             }}
+        >
+          <Box
+            sx={{
+              //Caja de foto y nombre
+              backgroundColor: COLOR.blanco,
+              width: "100%",
+              height: "20%",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <PersonIcon sx={{ fontSize: 100 }} />
+            <Typography sx={{ fontWeight: "Bold" }}>Cesar Garza</Typography>
+          </Box>
+          <Box
+            sx={{
+              //cuadro que contiene al cuadro de textos 1
+              backgroundColor: COLOR.blanco,
+              width: "100%",
+              height: "80%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Box
+              sx={{
+                backgroundColor: "white",
+                width: "80%",
+                height: "70%",
+                //Cuadro de textos 1
+              }}
+            >
+              <Divider
+                sx={{
+                  mt: 2,
+                  backgroundColor: COLOR.grisDivisionEntreElementos,
+                }}
+              />
+              <Box sx={{ display: "flex", mt: 2 }}>
+                <Typography sx={{ fontWeight: "Bold" }}>Area:</Typography>
+                <Typography>Coordinación de Planeación Hacendaria</Typography>
+              </Box>
+              <Divider
+                sx={{
+                  mt: 2,
+                  backgroundColor: COLOR.grisDivisionEntreElementos,
+                }}
+              />
+              <Box sx={{ display: "flex", mt: 2 }}>
+                <Typography sx={{ fontWeight: "Bold" }}>Puesto:</Typography>
+                <Typography>Coordinador</Typography>
+              </Box>
+              <Divider
+                sx={{
+                  mt: 2,
+                  backgroundColor: COLOR.grisDivisionEntreElementos,
+                }}
+              />
+              <Box sx={{ display: "flex", mt: 2 }}>
+                <Typography sx={{ fontWeight: "Bold" }}>Perfil:</Typography>
+                <Typography>Usuario</Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            //Espacio entre Boxes
+            backgroundColor: COLOR.blanco,
+            width: "2%",
+            height: "90%",
+          }}
+        >
+        </Box>
+        <Box
+          sx={{
+            //Editar informacion
+            backgroundColor: COLOR.blanco,
+            width: "68%",
+            height: "90%",
+            border:1,
+            borderBottom:0,
+            justifyContent: "center",
+          }}
+        >
+          <Box
+            sx={{ //Header de Editar contenido
+              width: "100%", 
+              height: "10%",
+              backgroundColor: COLOR.doradoNL,
+              display: "flex",
+              alignItems: "center",
+            }}
+          ><Typography variant="h4" sx={{
+            //Perfil
+            color: COLOR.blanco,
+          }}>Editar Información de Contacto</Typography>
+            
+          </Box>
+          <Box
+            sx={{
+              //Padre del espacio 3 después del vacío
+              height: "90%",
+              width: "100%",
+              backgroundColor:  COLOR.blanco,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Box
+              sx={{
+                height: "95%",
+                width: "95%",
+                backgroundColor:  COLOR.blanco,
+              }}
+            >
+              <TextField
+                sx={{ width: tamanioTextField, mt: 3 }}
+                id="telefono"
+                label="Teléfono"
+                variant="outlined"
+              />
+
+              <TextField
+                sx={{ width: tamanioTextField, mt: 3 }}
+                id="ubicacion"
+                label="Ubicación"
+                variant="outlined"
+              />
+              
+              <TextField
+                sx={{ width: tamanioTextField, mt: 3 }}
+                type="file"
+                id="telefono"
+                variant="outlined"
+              />
+                <Box sx={{width:"20%"}}>
+                <Fab sx={{ mt: 6 }} variant="extended">
+                <SaveIcon sx={{ mr: 1 }} />
+                Actualizar
+              </Fab>
+                </Box>
+              
+            </Box>
+          </Box>
+        </Box>
+      </Box>
     </Box>
-    );
+  );
 }
