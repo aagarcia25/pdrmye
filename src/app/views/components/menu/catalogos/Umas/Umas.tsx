2af8fa8121f6ec9657da29f49c6e661337c1a23d
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2022-09-14 14:07:33 -0500

    teste
    tes

--- src/app/views/components/menu/catalogos/Umas/Umas.tsx
+++ src/app/views/components/menu/catalogos/Umas/Umas.tsx
@@ -1,39 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, LinearProgress, Modal, TextField, Typography } from '@mui/material'
-import { DataGrid, esES, GridColDef } from '@mui/x-data-grid'
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  LinearProgress,
+  Modal,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { DataGrid, esES, GridColDef } from "@mui/x-data-grid";
 
-import { CustomNoRowsOverlay } from '../../CustomNoRowsOverlay'
-import { CustomToolbar } from '../../CustomToolbar'
-import { getUser } from '../../../../../services/localStorage'
-import { CatalogosServices } from '../../../../../services/catalogosServices'
-import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import AddIcon from '@mui/icons-material/Add';
-import { messages } from '../../../../styles'
+import { CustomNoRowsOverlay } from "../../CustomNoRowsOverlay";
+import { CustomToolbar } from "../../CustomToolbar";
+import { getUser } from "../../../../../services/localStorage";
+import { CatalogosServices } from "../../../../../services/catalogosServices";
+import ModeEditOutlineIcon from "@mui/icons-material/ModeEditOutline";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import AddIcon from "@mui/icons-material/Add";
+import { messages } from "../../../../styles";
+import UmasModel from "./UmasModel";
+import ButtonsAdd from "../Utilerias/ButtonsAdd";
+import Swal from "sweetalert2";
+import { Toast } from "../../../../../helpers/Toast";
+import { Alert } from "../../../../../helpers/Alert";
+
+
 
 export const Umas = () => {
-    
-
-    
-    
-
-
-
-
-
   const user = getUser();
+  const [modo, setModo] = useState("");
+  const [open, setOpen] = useState(false);
+  const [tipoOperacion, setTipoOperacion] = useState(0);
+ 
+  const [slideropen, setslideropen] = useState(false);
+  const [vrows, setVrows] = useState({});
   const [conUmas, setUmas] = useState([]);
 
-  const [open, setOpen] = useState(false);
-
-const columns: GridColDef[] = [
-   
-   
+  const columns: GridColDef[] = [
+    {
+      field: "id",
+      headerName: "Identificador",
+      hide: true,
+      width: 150,
+      description: messages.dataTableColum.id,
+    },
     { field: "Anio", headerName: "Año", width: 150 },
     { field: "Diario", headerName: "Diario", width: 150 },
     { field: "Mensual", headerName: "Mensual", width: 150 },
     { field: "Anual", headerName: "Anual", width: 150 },
-   
+
     {
       field: "acciones",
       headerName: "Acciones",
@@ -43,115 +63,140 @@ const columns: GridColDef[] = [
       renderCell: (v) => {
         return (
           <Box>
-            <IconButton onClick={() => handleOpen(v)}>
+            <IconButton onClick={() => handleEdit(v)}>
               <ModeEditOutlineIcon />
             </IconButton>
-            <IconButton onClick={() => handleOpen(v)}>
+            <IconButton onClick={() => handleDelete(v)}>
               <DeleteForeverIcon />
             </IconButton>
           </Box>
         );
       },
     },
-   
   ];
-  
+
+  const handleClose = () => {
+    setOpen(false);
+    consulta({NUMOPERACION: 4 })
+
+  };
+
   const handleOpen = (v: any) => {
-    //setSelectedId(v.row.lastName);
-
+    setTipoOperacion(1);
+    setModo("Agregar Registro");
     setOpen(true);
+    setVrows("");
   };
 
-  const handleClose = () => setOpen(false);
-
-  const ButtonAdd = () =>{
-    return (
-   <Box>
-     <IconButton color="primary"  onClick={() => handleOpen(1)}>
-           <AddIcon />
-      </IconButton>
-   </Box>
-    );
-  }
-
-
-  const DetailsModal = () => {
-    return (
-      <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Subscribe</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          To subscribe to this website, please enter your email address here. We
-          will send updates occasionally.
-        </DialogContentText>
-        <TextField
-          autoFocus
-          margin="dense"
-          id="name"
-          label="Email Address"
-          type="email"
-          fullWidth
-          variant="standard"
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Cancel</Button>
-        <Button onClick={handleClose}>Subscribe</Button>
-      </DialogActions>
-    </Dialog>
-    );
+  const handleEdit = (v: any) => {
+    console.log(v)
+    setTipoOperacion(2);
+    setModo("Editar Registro");
+    setOpen(true);
+    setVrows(v);
   };
 
-  
+  const handleDelete = (v: any) => {
+    Swal.fire({
+      icon: "info",
+      title: "Estas seguro de eliminar este registro?",
+      showDenyButton: true,
+      showCancelButton: false,
+      confirmButtonText: "Confirmar",
+      denyButtonText: `Cancelar`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        console.log(v);
 
+        let data = {
+          NUMOPERACION: 3,
+          CHID: v.row.id,
+          CHUSER: 1,
+        };
+        console.log(data);
 
-   
-  
-    let data = ({
-      NUMOPERACION: 4,
-      CHID: "",
-      NUMANIO: "",
-      NUMTOTALPOB: "",
-      CHUSER:1
-    })
-  
-  
-    useEffect(() => {
-      CatalogosServices.umas(data).then((res) => {
-      //  console.log(res);
+        CatalogosServices.umas(data).then((res) => {
+          if (res.SUCCESS) {
+            Toast.fire({
+              icon: "success",
+              title: "Registro Eliminado!",
+            });
+
+            consulta({NUMOPERACION: 4 });
+
+          } else {
+            Alert.fire({
+              title: "Error!",
+              text: res.STRMESSAGE,
+              icon: "error",
+            });
+          }
+        });
+
+      } else if (result.isDenied) {
+        Swal.fire("No se realizaron cambios", "", "info");
+      }
+    });
+  };
+
+  const consulta = (data: any) => {
+    CatalogosServices.umas(data).then((res) => {
+      if (res.SUCCESS) {
+        Toast.fire({
+          icon: "success",
+          title: "Consulta Exitosa!",
+        });
         setUmas(res.RESPONSE);
-      });
-    }, []);
+      } else {
+        Alert.fire({
+          title: "Error!",
+          text: res.STRMESSAGE,
+          icon: "error",
+        });
+      }
+    });
+  };
 
 
 
+
+  useEffect(() => {
+    consulta({NUMOPERACION: 4 })
+  }, []);
 
 
 
   return (
+    <div style={{ height: 600, width: "100%" }}>
+      {open ? (
+        <UmasModel
+          open={open}
+          modo={modo}
+          tipo={tipoOperacion}
+          handleClose={handleClose}
+          dt={vrows}
+        />
+      ) : (
+        ""
+      )}
 
+      <ButtonsAdd handleOpen={handleOpen} />
 
-    <div style={{ height: 600, width: "100%" }} >
-        <DetailsModal />
-    <ButtonAdd/>    
-    <DataGrid
-      //checkboxSelection
-      pagination
-      localeText={esES.components.MuiDataGrid.defaultProps.localeText}
-      components={{
-        Toolbar: CustomToolbar,
-        LoadingOverlay: LinearProgress ,
-        NoRowsOverlay: CustomNoRowsOverlay,
-      }}
-      rowsPerPageOptions={[5,10,20,50,100]}
-      rows={conUmas}
-      columns={columns}
-      
-     // loading //agregar validacion cuando se esten cargando los registros
-    />
-  </div>
+      <DataGrid
+        //checkboxSelection
+        pagination
+        localeText={esES.components.MuiDataGrid.defaultProps.localeText}
+        components={{
+          Toolbar: CustomToolbar,
+          LoadingOverlay: LinearProgress,
+          NoRowsOverlay: CustomNoRowsOverlay,
+        }}
+        rowsPerPageOptions={[5, 10, 20, 50, 100]}
+        rows={conUmas}
+        columns={columns}
 
-  
-  )
-}
-
+        // loading //agregar validacion cuando se esten cargando los registros
+      />
+    </div>
+  );
+};
