6c93fce63f952b9bc56a5c1aa113019ff75edf4d
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2022-09-14 14:58:53 -0500

    Update MunFacturacion.tsx

--- src/app/views/components/menu/catalogos/MunFactguracion/MunFacturacion.tsx
+++ src/app/views/components/menu/catalogos/MunFactguracion/MunFacturacion.tsx
@@ -104,6 +104,7 @@ export const MunFacturacion = () => {
   };
 
   const handleOpen = (v: any) => {
+    console.log('cerrando');
     setTipoOperacion(1);
     setModo("Agregar Registro");
     setOpen(true);
@@ -111,6 +112,8 @@ export const MunFacturacion = () => {
   };
 
   const handleEdit = (v: any) => {
+
+    console.log('cerrando');
     console.log(v)
     setTipoOperacion(2);
     setModo("Editar Registro");
