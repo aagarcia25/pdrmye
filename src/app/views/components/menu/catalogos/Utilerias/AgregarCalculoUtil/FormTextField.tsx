a27ec609561afba373ed029fa17358c3b61b1f1f
gcantre <gcantu@cecapmex.com>
2022-09-14 11:33:52 -0500

    FFM70

--- src/app/views/components/menu/catalogos/Utilerias/AgregarCalculoUtil/FormTextField.tsx
+++ src/app/views/components/menu/catalogos/Utilerias/AgregarCalculoUtil/FormTextField.tsx
@@ -10,7 +10,7 @@ export function FormTextField(props: FormTextFieldProps) {
   return (
     <>
       <Grid item xs={3} sx={{ display: "flex", justifyContent: "center" }}>
-        <Typography sx={{ fontWeight: "Bold" }}>{props.text}</Typography>
+        <Typography sx={{ fontWeight: "Bold" }}>{props.text}:</Typography>
       </Grid>
       <Grid item xs={3}>
         <Input placeholder={props.inputPlaceholder}></Input>
