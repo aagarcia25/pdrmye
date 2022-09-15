5c3d5f523da0fdd57c45a5ba5faa4ed339e801c4
gcantre <gcantu@cecapmex.com>
2022-09-15 09:36:56 -0500

    Componente AnioReadOnly y trae el año actual

---
+++ src/app/views/components/menu/catalogos/Utilerias/AgregarCalculoUtil/AnioReadOnly.tsx
@@ -0,0 +1,17 @@
+import { Grid, Input, Typography } from "@mui/material";
+
+export function AnioReadOnly(){
+
+    let year:number= new Date().getFullYear();
+
+    return(<>
+        <Grid item xs={3} sx={{ display: "flex", justifyContent: "center" }}>
+          <Typography sx={{ fontWeight: "Bold" }}>Año:</Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Input readOnly defaultValue={year}></Input>
+        </Grid>
+        <Grid item xs={6}></Grid>
+      </>
+      );
+}
