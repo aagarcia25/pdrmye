a09b785f36b90f929853e2e16d7b959eeb9c8df2
gcantre <gcantu@cecapmex.com>
2022-09-26 13:28:46 -0500

    Calendario horario
    Cambiado el horario que esta definido como de 9 am a 6 pm.

--- src/app/views/components/CalendarC.tsx
+++ src/app/views/components/CalendarC.tsx
@@ -1,64 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import { render } from "react-dom";
 import { localizer } from "../../helpers/calendarLocalizer";
 import { getMessagesES } from "../../helpers/getMessages";
-
 import { CalendarioService } from "../../services/calendarioService";
 import moment from "moment";
-import { calendario, eventoc, RESPONSE } from "../../interfaces/calendario/calendario";
+import {
+  calendario,
+  eventoc,
+  RESPONSE,
+} from "../../interfaces/calendario/calendario";
+import { previousDay } from "date-fns";
 
 const CalendarC = () => {
+
   const [eventos, setEventos] = useState<eventoc[]>();
 
-  const location = momentLocalizer(moment);
+  const today = new Date();
 
-  const pruebaEventos = [
-    {
-      id: 1,
-      title: "titulo",
-      start: "2022-09-14T10:00:00",
-      end: "2022-09-14T12:00:00",
-    },
-    {
-      id: 2,
-      title: "rompe",
-      start: "2022-09-14T10:00:00",
-      end: "2022-09-14T12:00:00",
-    },
-  ];
+  const onSelectEvent = () => {
+    
+    alert("Evento seleccionado");
+  };
 
-  const eventosFormato = eventos?.map((item: any) => {
-    return {
-      id: item.id,
-      title: item.NombreEvento,
-      start: item.InicioEvento,
-      end: item.FinEvento,
-    };
-  });
+  const handleSelectSlot = () =>{
+    console.log("Slot clicked");
+  }
 
   useEffect(() => {
     CalendarioService.calendarios({ NUMOPERACION: "4", CHUSER: "1" }).then(
       (res: any) => {
-        
         const even: calendario = res;
         //setEventos(even.RESPONSE);
         let eveitem: eventoc[] = [];
         even.RESPONSE.map((item: RESPONSE) => {
-        
           let it = {
             id: item.id,
             title: item.NombreEvento,
             allDay: true,
             start: new Date(item.InicioEvento),
             end: new Date(item.FinEvento),
-          }
-          eveitem.push(it)
+          };
+          eveitem.push(it);
         });
         console.log(eveitem);
         setEventos(eveitem);
-
       }
     );
   }, []);
@@ -76,14 +62,32 @@ const CalendarC = () => {
         height: "calc( 80vh - 80px )",
       }}
       messages={getMessagesES()}
+      onSelectEvent={onSelectEvent}
+      onSelectSlot={handleSelectSlot}
+      selectable
       //  eventPropGetter={ eventStyleGetter }
       //   components={{
       //     event: CalendarEvent
       //    }}
-      //   onDoubleClickEvent={ onDoubleClick }
-      // onSelectEvent={ onSelect }
       //  onView={ onViewChanged }
       //TODO No se ven los eventos por vista semana o día
+      min={
+        new Date(
+          today.getFullYear(), 
+          today.getMonth(), 
+          today.getDate(), 
+          9
+        )
+      }
+  // end time 5:00pm
+     max={
+       new Date(
+         today.getFullYear(), 
+         today.getMonth(), 
+         today.getDate(), 
+         18
+       )
+     }
     />
   );
 };
