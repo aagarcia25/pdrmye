39c84e2f4a4db6e761d3ff6e750e2feaa031dc58
abelinoolguin0402 <112125177+abelinoolguin0402@users.noreply.github.com>
2022-10-03 18:01:40 -0500

    ajustes
    ajustes

--- src/app/views/components/ListNotification.tsx
+++ src/app/views/components/ListNotification.tsx
@@ -1,63 +1,46 @@
-import React, {useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Box,
   Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
   IconButton,
-  LinearProgress,
-  Modal,
-  TextField,
-  Typography,
 } from "@mui/material";
 import { GridColDef } from "@mui/x-data-grid";
-
-import { CustomToolbar } from "./menu/CustomToolbar";
-
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-import AddIcon from "@mui/icons-material/Add";
-
 import VisibilityIcon from "@mui/icons-material/Visibility";
-import { CustomNoRowsOverlay } from "./menu/CustomNoRowsOverlay";
-import { messages } from "../styles";
-import { getPU, getUser } from "../../services/localStorage";
+import { getPU } from "../../services/localStorage";
 import { CatalogosServices } from "../../services/catalogosServices";
-
 import MUIXDataGrid from "./MUIXDataGrid";
-import ListNotificationsModal from "./ListNotificationsModal";
 import { UserReponse } from "../../interfaces/user/UserReponse";
-
-
-  
-
-
-
+import AddIcon from '@mui/icons-material/Add';
+import SendIcon from '@mui/icons-material/Send';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import InboxIcon from '@mui/icons-material/Inbox';
+import ListNotificationsModal from "./ListNotificationsModal";
 
 
 export const ListNotification = () => {
-  
-  
+
+
 
   const [modo, setModo] = useState("");
   const [tipoOperacion, setTipoOperacion] = useState(0);
   const [data, setData] = useState({});
-
   const user: UserReponse = JSON.parse(String(getPU()));
   const [notificacion, setNotificacion] = useState([]);
   const [open, setOpen] = useState(false);
+  const [chuser, setChuser] = useState("");
 
   const columns: GridColDef[] = [
-    {field: "id", headerName: "Identificador", width: 150, hide:true},
-    { field: "Descripcion", headerName: "Mensage", width: 300, resizable:true},
-    { field: "Visto", headerName: "Leido", width: 300,},
-    { field: "acciones",
+    { field: "id", headerName: "Identificador", width: 150, hide: true },
+    { field: "Descripcion", headerName: "Mensage", width: 300, resizable: true },
+    { field: "Visto", headerName: "Leido", width: 300, },
+    {
+      field: "acciones",
       headerName: "Acciones",
       description: "Campo de Acciones",
       sortable: false,
-      width: 200,      
+      width: 200,
       renderCell: (v) => {
         return (
           <Box>
@@ -79,13 +62,19 @@ export const ListNotification = () => {
     setOpen(true);
     setData(v);
   };
+  const handleNuevoMensaje = () => {
+    setTipoOperacion(1);
+    setModo("Agregar Aviso");
+    setOpen(true);
+    setChuser(String(user.IdUsuario));
 
+  };
 
   const changeView = (v: any) => {
     let data = {
-      NUMOPERACION:6,
-      CHID:v.row.id,
-      CHUSER:user.IdUsuario
+      NUMOPERACION: 6,
+      CHID: v.row.id,
+      CHUSER: user.IdUsuario
     };
     CatalogosServices.Notificaciones(data).then((res) => {
       setNotificacion(res.RESPONSE);
@@ -94,46 +83,14 @@ export const ListNotification = () => {
 
 
   const handleClose = () => setOpen(false);
- 
 
-  const DetailsModal = () => {
-    return (
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here.
-            We will send updates occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
-        </DialogActions>
-      </Dialog>
-    );
-  };
-
-
-  
   let dat = {
-    NUMOPERACION:4,
-    CHID:1,
-    DESCRIPCION:'',
-    DESTINATARIO:'',
-    MODIFICADOPOR:1,
-    CREADOPOR:1,
-    DELETED:'',
-    CHUSER:user.IdUsuario
+    NUMOPERACION: 4,
+    CHID: 1,
+    MODIFICADOPOR: 1,
+    CREADOPOR: 1,
+    DELETED: '',
+    CHUSER: user.IdUsuario
 
   };
 
@@ -145,35 +102,77 @@ export const ListNotification = () => {
 
   return (
     <div style={{ height: 600, width: "100%" }} >
-    {open ? (
-      <ListNotificationsModal
-        open={open}
-        modo={modo}
-        handleClose={handleClose}
-        tipo={tipoOperacion}
-        dt={data}
-      />
-    ) : (
-      ""
-    )}
 
-    
-    <Box sx={{
-      display: 'flex',
-      justifyContent: 'content-position',
-      p: 1,
-      m: 1,
-      bgcolor: 'background.paper',
-      borderRadius: 2,
-    }} >
-    <Box sx={{ height: 600, width: "900px", borderRadius: 3}}>
-      <MUIXDataGrid 
-      columns={columns} rows={notificacion} />
+        {open ? (
+        <ListNotificationsModal
+          open={open}
+          modo={modo}
+          handleClose={handleClose}
+          tipo={tipoOperacion}
+          dt={data}
+        />
+      ) : (
+        ""
+      )}
+
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'content-position',
+        p: 1,
+        m: 1,
+        bgcolor: 'background.paper',
+        borderRadius: 2,
+      }} >
+
+        <Box sx={{  bgcolor: 'rgb(245, 245, 245)', height: 600, width: "150px", borderRadius: 3 }}>
+        
+        <Box sx={{ position: 'relative', top: 10,left: 7,width: "90%",justifyContent:'center',  display: 'flex',borderRadius: 1 }}>
+                    
+          <Button className="nuevo-mensaje" color="success" variant="contained" endIcon={<AddIcon />}
+          onClick={() => handleNuevoMensaje()}>
+            Nuevo</Button>
+                    
+          </Box>
+
+          <Box sx={{ 
+             height: "120px",
+           justifyContent: 'space-between',
+          position: 'relative',
+          flexDirection: 'column', 
+          top: 50,left: 7,width: "90%",
+          display: 'flex',
+          borderRadius: 1 }}>
+
+          <Button className="notificaciones-generico" color="success"  startIcon={<SendIcon />}>
+            Enviados</Button>
+            <Button className="notificaciones-generico" color="success" startIcon ={<InboxIcon />}>
+            Recibidos</Button>
+            <Button className="notificaciones-generico" color="success"  startIcon={<AutoStoriesIcon />}>
+             Leidos </Button>
+         
+
+
+          </Box>
+        
+
+
+
+
+
+
+        </Box>
+
+
+
+
+        <Box sx={{ height: 600, width: "900px", borderRadius: 3 }}>
+          <MUIXDataGrid
+            columns={columns} rows={notificacion} />
+        </Box>
+
+
       </Box>
-
-
-   </Box>
-   </div>
+    </div>
   );
 };
 
--- src/app/views/components/ListNotificationsModal.tsx
+++ src/app/views/components/ListNotificationsModal.tsx
@@ -8,6 +8,14 @@ import {
   Container,
   IconButton,
   FormLabel,
+  InputLabel,
+  Input,
+  Select,
+  OutlinedInput,
+  MenuItem,
+  SelectChangeEvent,
+  FormControl,
+  Button,
 } from "@mui/material";
 import UploadFileIcon from '@mui/icons-material/UploadFile';
 import imagenGenerica from '../../../../../../app/assets/img/archivoImagen.jpg'
@@ -15,13 +23,17 @@ import PdfLogo from '../../../../../../app/assets/img/PDF_file_icon.svg'
 import PptxLogo from '../../../../../../app/assets/img/pptx_Logo.png'
 import xlsxLogo from '../../../../../../app/assets/img/xlsx_Logo.png'
 import docxLogo from '../../../../../../app/assets/img/docx_Logo.png'
-
+import CloseIcon from '@mui/icons-material/Close';
 import { ListNotification } from "./ListNotification";
 import { Imunicipio } from "../../interfaces/municipios/FilterMunicipios";
 import { getMunicipios, setMunicipios, validaLocalStorage } from "../../services/localStorage";
 import { CatalogosServices } from "../../services/catalogosServices";
 import { Toast } from "../../helpers/Toast";
 import { Alert } from "../../helpers/Alert";
+import { Label } from "@mui/icons-material";
+import { borderColor } from "@mui/system";
+import SendIcon from '@mui/icons-material/Send';
+
 
 
 const ListNotificationsModal = ({
@@ -37,6 +49,32 @@ const ListNotificationsModal = ({
   handleClose: Function,
   dt: any
 }) => {
+  const names = [
+    'Oliver Hansen',
+    'Van Henry',
+    'April Tucker',
+    'Ralph Hubbard',
+    'Omar Alexander',
+    'Carlos Abbott',
+    'Miriam Wagner',
+    'Bradley Wilkerson',
+    'Virginia Andrews',
+    'Kelly Snyder',
+  ];
+  const [personName, setPersonName] = React.useState<string[]>([]);
+  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
+
+
+
   const [tipoOperacion, setTipoOperacion] = useState(0);
   const [data, setData] = useState({});
   ////////////////////////////
@@ -48,7 +86,7 @@ const ListNotificationsModal = ({
     //console.log("fin de evento   " + finEvento);
     //console.log("noombre de evento    " + nameAviso);
     ///console.log("fecha de hoy   " + Fecha_min);
-    console.log("datos de dt  " + dt?.row + " ---- "+ mensaje); 
+    console.log("datos de dt  " + dt?.row + " ---- " + mensaje);
 
   }
   const handleRequest = (data: any) => {
@@ -103,7 +141,7 @@ const ListNotificationsModal = ({
     if (dt === '') {
 
     } else {
-      setId(dt?.row?.id);      
+      setId(dt?.row?.id);
       setMensaje(dt?.row?.Descripcion);
 
     }
@@ -120,27 +158,121 @@ const ListNotificationsModal = ({
       open={open}
 
     >
-      <DialogTitle>Notificaciones</DialogTitle>
-      <Box>
-        <Box>
 
-          <FormLabel focused>
-            {mensaje}            
-          </FormLabel>
+      <Box sx={{
+        height: "100%",
+        justifyContent: 'space-between',
+        position: 'relative',
+        flexDirection: 'column',
+        borderRadius: 1
+      }}>
+        <Box>
+          <Box sx={{
+            height: "100%",
+            justifyContent: 'space-between',
+            position: 'relative',
+            flexDirection: 'column',
+
+            display: 'flex',
+            borderRadius: 1
+          }}>
+            <Box sx={{
+
+              display: 'flex',
+              justifyContent: 'space-between',
+              position: 'relative',
+
+              borderRadius: 1,
+
+            }}>
+              <Box sx={{
+                position: 'relative',
+                flexDirection: 'column',
+                top: 1, left: 20,
+                borderRadius: 1
+              }}>
+                <FormLabel
+                  focused
+                > <h3> Nuevo Mensaje</h3>
+                </FormLabel>
+              </Box>
+              <Box>
+                <button className="cerrar-nuevo-mensaje" color="error"
+                  onClick={() => handleClose()}>
+                  <CloseIcon />
+                </button>
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                height: "120px",
+                justifyContent: 'space-between',
+                position: 'relative',
+                flexDirection: 'column',
+                top: 10, left: 7, width: "95%",
+                display: 'flex',
+                borderRadius: 1
+              }}>
+
+
+              <FormControl sx={{ m: 1, width: "50%" }}>
+                <InputLabel id="demo-multiple-checkbox-label">Para</InputLabel>
+                <Select
+                  labelId="demo-multiple-name-label"
+                  id="demo-multiple-name"
+                  multiple
+                  value={personName}
+                  onChange={handleChange}
+                  input={<OutlinedInput label="Para" />}
+
+                >
+                  {names.map((name) => (
+                    <MenuItem
+                      key={name}
+                      value={name}
+
+                    >
+                      {name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+
+
+            </Box>
+
+            <Box sx={{
+              width: "98%",
+              position: 'relative',
+                          
+              left: 5,
+              flexDirection: 'column',
+              borderRadius: 1,
+              bgcolor: "rgb(245,245,245)",
+              borderColor: "rgb(255,240,225)",
+            }}>
+              <Input
+                multiline
+                placeholder="Mensaje"
+                sx={{ m: 1, width: "98%", height: "150px" }} />
+            </Box>
+
+
+          </Box>
+          <Box sx={{  display: 'flex',
+          flexDirection: 'row-reverse',}}   >
+              <Box sx={{  width: "75px",
+            
+}} >
+            <Button    
+            className="enviar-mensaje" color="success" variant="contained" endIcon={<SendIcon />}
+              onClick={() => handleClose()}>
+              Enviar</Button>
+          
+              </Box>
+          </Box>
 
         </Box>
-        <Box sx={{ bgcolor: 'rgb(255, 255, 255)', width: '100%', display: 'flex', flexDirection: 'row-reverse', }}>
-
-<button className="button cerrar" onClick={() => handleClose()}  >Cerrar</button>
-
-<button className="button cerrar" onClick={() => testeoVariables()}  >test</button>
-
-
-
-
-
-</Box>
-
       </Box>
     </Dialog>
 
