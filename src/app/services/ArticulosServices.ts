930951b7f5847781c8f416f711d6dca5329e9e54
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2022-10-06 16:45:58 -0500

    ajustes
    ajustes

--- src/app/services/ArticulosServices.ts
+++ src/app/services/ArticulosServices.ts
@@ -6,7 +6,5 @@ export class ArticulosServices{
         return await post('articulof1', data);
     }
 
-    public static async articulof2(data: any) {
-        return await post('articulof2', data);
-    }
+   
 }
