eb251723109d509be0040e3845d121177f093dd8
gcantre <gcantu@cecapmex.com>
2022-10-18 17:39:58 -0500

    ajuste

--- src/app/views/components/menu/catalogos/ParametrosGenerales/ParametrosGenerales.tsx
+++ src/app/views/components/menu/catalogos/ParametrosGenerales/ParametrosGenerales.tsx
@@ -12,200 +12,169 @@ import Slider from "../../../Slider";
 import Buttons from "../Utilerias/Buttons";
 import MUIXDataGrid from "../../../MUIXDataGrid";
 import { ParametrosGeneralesModal } from "./ParametrosGeneralesModal";
+import { Box, IconButton } from "@mui/material";
+import ModeEditOutlineIcon from "@mui/icons-material/ModeEditOutline";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import ButtonsAdd from "../Utilerias/ButtonsAdd";
 
-export const ParametrosGenerales= ()=>{
-    const [parametroGeneral, setParametroGeneral] = useState([]);
+export const ParametrosGenerales = () => {
+  const [parametroGeneral, setParametroGeneral] = useState([]);
+  const [modo, setModo] = useState("");
+  const [open, setOpen] = useState(false);
+  const [tipoOperacion, setTipoOperacion] = useState(0);
+  const [vrows, setVrows] = useState({});
 
+  console.log("parametroGeneral", parametroGeneral);
 
-    const [modo, setModo] = useState("");
-  
-    const [open, setOpen] = useState(false);
-    const [tipoOperacion, setTipoOperacion] = useState(0);
-    const [data, setData] = useState({});
-    const [plantilla, setPlantilla] = useState("");
-    const [slideropen, setslideropen] = useState(false);
-    const user: RESPONSE = JSON.parse(String(getUser()));
+  const columns: GridColDef[] = [
+    {
+      field: "id",
+      hide: true,
+      headerName: "Identificador",
+      width: 150,
+      description: messages.dataTableColum.id,
+    },
+    {
+      field: "Nombre",
+      headerName: "Nombre",
+      width: 150,
+    },
+    {
+      field: "Valor",
+      headerName: "Valor",
+      width: 120,
+    },
+    {
+      field: "acciones",
+      headerName: "Acciones",
+      description: "Campo de Acciones",
+      sortable: false,
+      width: 150,
+      renderCell: (v) => {
+        return (
+          <Box>
+            <IconButton onClick={() => handleEdit(v)}>
+              <ModeEditOutlineIcon />
+            </IconButton>
+            <IconButton onClick={() => handleDelete(v)}>
+              <DeleteForeverIcon />
+            </IconButton>
+          </Box>
+        );
+      },
+    },
+  ];
+  console.log("Columns: ", columns);
 
-    const columns: GridColDef[] = [
-        {
-          field: "id",
-          hide: true,
-          headerName: "Identificador",
-          width: 150,
-          description: messages.dataTableColum.id,
-        },
-        {
-          field: "Nombre",
-          headerName: "Nombre",
-          width: 150,
-        },
-        {
-          field: "Valor",
-          headerName: "Valor",
-          width: 120,
-        },
-        {
-          field: "acciones",
-          headerName: "Acciones",
-          description: "Campo de Acciones",
-          sortable: false,
-          width: 150,
-          renderCell: (v) => {
-            return (
-              <AccionesGrid
-                handleEditar={handleEdit}
-                handleBorrar={handleDelete}
-                v={v}
-                update={true}
-                pdelete={true}
-              />
-            );
-          },
-        },
-      ];
+  const handleClose = () => {
+    setOpen(false);
+    consulta({ NUMOPERACION: 4 });
+  };
 
-      const handleClose = () => {
-        setOpen(false);
-        consulta({ NUMOPERACION: 4 });
-      };
+  const handleOpen = (v: any) => {
+    setTipoOperacion(1);
+    setModo("Agregar Registro");
+    setOpen(true);
+    setVrows("");
+  };
 
-      const handleOpen = (v: any) => {
-        setTipoOperacion(1);
-        setModo("Agregar Registro");
-        setOpen(true);
-        setData("");
-      };
-    
-      const handleEdit = (v: any) => {
-        setTipoOperacion(2);
-        setModo("Editar Registro");
-        setOpen(true);
-        setData(v);
-      };
-    
-      const handleDelete = (v: any) => {
-        Swal.fire({
-          icon: "info",
-          title: "Estas seguro de eliminar este registro?",
-          showDenyButton: true,
-          showCancelButton: false,
-          confirmButtonText: "Confirmar",
-          denyButtonText: `Cancelar`,
-        }).then((result) => {
-          if (result.isConfirmed) {
-            console.log(v);
-            const user: RESPONSE = JSON.parse(String(getUser()));
-    
-            let data = {
-              NUMOPERACION: 3,
-              CHID: v.row.id,
-              CHUSER: user.id
-            };
-            console.log(data);
-    
-            ParametroServices.ParametroGeneralesIndex(data).then((res) => {
-              if (res.SUCCESS) {
-                Toast.fire({
-                  icon: "success",
-                  title: "Registro Eliminado!",
-                });
-    
-                let data = {
-                  NUMOPERACION: 4,
-                };
-                consulta(data);
-              } else {
-                Alert.fire({
-                  title: "Error!",
-                  text: res.STRMESSAGE,
-                  icon: "error",
-                });
-              }
-            });
-          } else if (result.isDenied) {
-            Swal.fire("No se realizaron cambios", "", "info");
-          }
-        });
-      };
-    
-      const handleUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setslideropen(true);
-        let file = event?.target?.files?.[0] || "";
-        const formData = new FormData();
-        formData.append("inputfile", file, "inputfile.xlsx");
-        formData.append("tipo", "MunFacturacion");
-        ParametroServices.ParametroGeneralesIndex(formData).then((res) => {
-          setslideropen(false);
-          if (res.SUCCESS) {
-            Toast.fire({
-              icon: "success",
-              title: "Carga Exitosa!",
-            });
-          } else {
-            Alert.fire({
-              title: "Error!",
-              text: res.STRMESSAGE,
-              icon: "error",
-            });
-          }
-        });
-      };
-    
-      const consulta = (data: any) => {
-        ParametroServices.ParametroGeneralesIndex(data).then((res) => {
-          if (res.SUCCESS) {
-            Toast.fire({
-              icon: "success",
-              title: "Consulta Exitosa!",
-            });
-            console.log(data);
-            setParametroGeneral(res.RESPONSE);
-          } else {
-            Alert.fire({
-              title: "Error!",
-              text: res.STRMESSAGE,
-              icon: "error",
-            });
-            console.log(res);
-            console.log(res.SUCCESS);
-          }
-        });
-      };
-    
-      useEffect(() => {
-        console.log();
+  const handleEdit = (v: any) => {
+    console.log(v)
+    setTipoOperacion(2);
+    setModo("Editar Registro");
+    setOpen(true);
+    setVrows(v);
+  };
+
+  const handleDelete = (v: any) => {
+    Swal.fire({
+      icon: "info",
+      title: "Estas seguro de eliminar este registro?",
+      showDenyButton: true,
+      showCancelButton: false,
+      confirmButtonText: "Confirmar",
+      denyButtonText: `Cancelar`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        console.log(v);
+        const user: RESPONSE = JSON.parse(String(getUser()));
+
         let data = {
-          NUMOPERACION: 4,
+          NUMOPERACION: 3,
+          CHID: v.row.id,
+          CHUSER: user.id,
         };
-    
-        ParametroServices.ParametroGeneralesIndex(data).then((res) => {
-          setParametroGeneral(res.RESPONSE);
-        });
-      }, []);
+        console.log(data);
 
-      return(
-        <div style={{ height: 600, width: "100%" }}>
-        <Slider open={slideropen}></Slider>
+        ParametroServices.ParametroGeneralesIndex(data).then((res) => {
+          if (res.SUCCESS) {
+            Toast.fire({
+              icon: "success",
+              title: "Registro Eliminado!",
+            });
+
+            let data = {
+              NUMOPERACION: 4,
+            };
+            consulta(data);
+          } else {
+            Alert.fire({
+              title: "Error!",
+              text: res.STRMESSAGE,
+              icon: "error",
+            });
+          }
+        });
+      } else if (result.isDenied) {
+        Swal.fire("No se realizaron cambios", "", "info");
+      }
+    });
+  };
+
   
-        {open ? (
+
+  const consulta = (data: any) => {
+    ParametroServices.ParametroGeneralesIndex(data).then((res) => {
+      if (res.SUCCESS) {
+        Toast.fire({
+          icon: "success",
+          title: "Consulta Exitosa!",
+        });
+        console.log(data);
+        setParametroGeneral(res.RESPONSE);
+        console.log("parametroGeneral consulta", parametroGeneral);
+      } else {
+        Alert.fire({
+          title: "Error!",
+          text: res.STRMESSAGE,
+          icon: "error",
+        });
+      }
+    });
+  };
+
+  useEffect(() => {
+    consulta({ NUMOPERACION: 4 })
+  }, []);
+
+  return (
+    <div style={{ height: 600, width: "100%" }}>
+      {open ? (
         <ParametrosGeneralesModal
           open={open}
           modo={modo}
-          handleClose={handleClose}
           tipo={tipoOperacion}
-          dt={data}
+          handleClose={handleClose}
+          dt={vrows}
         />
       ) : (
         ""
       )}
 
-        <Buttons
-          handleOpen={handleOpen}
-          url={plantilla}
-          handleUpload={handleUpload}
-        />
-  
-        <MUIXDataGrid sx={{}} columns={columns} rows={parametroGeneral} />
-      </div>
-        );
-}
+      <ButtonsAdd handleOpen={handleOpen} />
+      <MUIXDataGrid columns={columns} rows={parametroGeneral} />
+
+
+    </div>
+  );
+};
--- src/app/views/components/menu/catalogos/ParametrosGenerales/ParametrosGeneralesModal.tsx
+++ src/app/views/components/menu/catalogos/ParametrosGenerales/ParametrosGeneralesModal.tsx
@@ -1,4 +1,11 @@
-import { Box, Dialog, DialogActions, DialogContent, TextField } from "@mui/material";
+import {
+  Box,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  TextField,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import { Alert } from "../../../../../helpers/Alert";
 import { Toast } from "../../../../../helpers/Toast";
@@ -25,8 +32,16 @@ export const ParametrosGeneralesModal = ({
   const [valor, setValor] = useState("");
   const user: RESPONSE = JSON.parse(String(getUser()));
 
+  //IMPRESIONES DE CAMPOS
+  console.log("---------Impresión de CAMPOS------");
+  console.log("id", id);
+  console.log("nombre", nombre);
+  console.log("valor", valor);
+  console.log("user", user);
+  console.log("---------FIN-de-Impresión de CAMPOS------");
+
   const handleSend = () => {
-    if (nombre || valor) {
+    if (nombre === null || valor === null) {
       Alert.fire({
         title: "Error!",
         text: "Favor de Completar los Campos",
@@ -34,15 +49,11 @@ export const ParametrosGeneralesModal = ({
       });
     } else {
       let data = {
+        CHID:id,
         NUMOPERACION: tipo,
-        CHID: id,
-        CHUSER: user.id,
         NOMBRE: nombre,
         VALOR: valor,
-        DELETED: 0,
       };
-      console.log("user props", user);
-      console.log("user id", user.id);
       console.log("data de modal", data);
       handleRequest(data);
       handleClose();
@@ -53,12 +64,9 @@ export const ParametrosGeneralesModal = ({
     console.log(data);
     if (tipo == 1) {
       //AGREGAR
-      console.log("A AGREGAR");
       agregar(data);
     } else if (tipo == 2) {
       //EDITAR
-      console.log("A EDITAR");
-
       editar(data);
     }
   };
@@ -89,14 +97,12 @@ export const ParametrosGeneralesModal = ({
           icon: "success",
           title: "Registro Editado!",
         });
-        console.log("Sé pudo editar");
       } else {
         Alert.fire({
           title: "Error!",
           text: res.STRMESSAGE,
           icon: "error",
         });
-        console.log("No se pudo editar");
       }
     });
   };
@@ -118,10 +124,12 @@ export const ParametrosGeneralesModal = ({
           <Box sx={{ display: "flex", justifyContent: "center" }}>
             <label className="Titulo">{modo}</label>
           </Box>
-          <TextField
+          {(modo === "Agregar Registro") ?
+            <Container maxWidth="sm">
+                <TextField
             required
             margin="dense"
-            id="nombre"
+            id="Nombre"
             label="Nombre"
             value={nombre}
             type="text"
@@ -134,7 +142,7 @@ export const ParametrosGeneralesModal = ({
           <TextField
             required
             margin="dense"
-            id="valor"
+            id="Valor"
             label="Valor"
             value={valor}
             type="text"
@@ -144,6 +152,42 @@ export const ParametrosGeneralesModal = ({
             error={valor ? true : false}
             InputProps={{}}
           />
+            </Container>
+          : ""
+          }
+
+{(modo === "Editar Registro") ?
+            <Container maxWidth="sm">
+                <TextField
+                disabled
+            required
+            margin="dense"
+            id="Nombre"
+            label="Nombre"
+            value={nombre}
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={(v) => setNombre(v.target.value)}
+            InputProps={{}}
+          />
+          <TextField
+            required
+            margin="dense"
+            id="Valor"
+            label="Valor"
+            value={valor}
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={(v) => setValor(v.target.value)}
+            error={valor ? true : false}
+            InputProps={{}}
+          />
+            </Container>
+          : ""
+          }
+          
         </Box>
       </DialogContent>
       <DialogActions>
