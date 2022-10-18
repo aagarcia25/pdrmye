5b4556f781a769884ad4486fe1d344fcf8d0035a
abelinoolguin0402 <112125177+abelinoolguin0402@users.noreply.github.com>
2022-10-18 09:55:16 -0500

    ajustes
    ajustes busqueda en menu

--- src/app/views/components/MUIXDataGridSimple.tsx
+++ src/app/views/components/MUIXDataGridSimple.tsx
@@ -1,24 +1,36 @@
 import * as React from "react";
-import { DataGrid,  esES as gridEsES, esES } from "@mui/x-data-grid";
+import { DataGrid,  esES as gridEsES, esES, GridCellParams, GridToolbar } from "@mui/x-data-grid";
 import { createTheme,ThemeProvider } from "@mui/material";
 import { esES as coreEsES } from "@mui/material/locale";
 
 const theme = createTheme(coreEsES, gridEsES);
 
-
-
 export default function MUIXDataGridSimple(props: any) {
+
   return (
       <ThemeProvider theme={theme}>
         <DataGrid
+          {...props.rows}
           density="compact"
+          
           disableSelectionOnClick 
-          rowsPerPageOptions={[5]}
+          disableColumnFilter
+          disableIgnoreModificationsIfProcessingProps	
+          disableColumnSelector
+          disableDensitySelector
+          rowsPerPageOptions={[6]}
+          components={{ Toolbar: GridToolbar }}
           pageSize={5}
           localeText={esES.components.MuiDataGrid.defaultProps.localeText}
           columns={props.columns}
           rows={props.rows}
           sx={{ fontFamily: "MontserratMedium" }}
+          componentsProps={{
+            toolbar: {
+              showQuickFilter: true,
+              quickFilterProps: { debounceMs: 500 },
+            },
+          }}
         />
       </ThemeProvider>
   );
