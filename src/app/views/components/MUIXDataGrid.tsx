e69ad99ed0bff3c66a22237932b8604870611027
abelinoolguin0402 <112125177+abelinoolguin0402@users.noreply.github.com>
2022-10-18 10:03:19 -0500

    ajustes
    busqueda en tabla

--- src/app/views/components/MUIXDataGrid.tsx
+++ src/app/views/components/MUIXDataGrid.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { DataGrid,  esES as gridEsES, esES } from "@mui/x-data-grid";
+import { DataGrid,  esES as gridEsES, esES, GridToolbar } from "@mui/x-data-grid";
 import { createTheme, LinearProgress, ThemeProvider } from "@mui/material";
 import { esES as coreEsES } from "@mui/material/locale";
 import { CustomNoRowsOverlay } from "./menu/CustomNoRowsOverlay";
@@ -14,17 +14,29 @@ export default function MUIXDataGrid(props: any) {
     <div style={{ height: "60vh", width: "100%" }}>
       <ThemeProvider theme={theme}>
         <DataGrid
+          {...props.rows}
           localeText={esES.components.MuiDataGrid.defaultProps.localeText}
           columns={props.columns}
           rows={props.rows}
           density="compact"
           rowsPerPageOptions={[10, 25, 50, 100]}
           disableSelectionOnClick 
+          disableColumnFilter
+          disableColumnSelector
+          disableDensitySelector
           sx={{ fontFamily: "MontserratMedium" }}
-          components={{
+          components={{ Toolbar: GridToolbar }}
+          /*components={{
             Toolbar: CustomToolbar,
             LoadingOverlay: LinearProgress,
-            NoRowsOverlay: CustomNoRowsOverlay,
+            NoRowsOverlay: CustomNoRowsOverlay,            
+          }}*/
+
+          componentsProps={{
+            toolbar: {
+              showQuickFilter: true,
+              quickFilterProps: { debounceMs: 500 },
+            },
           }}
          
         />
