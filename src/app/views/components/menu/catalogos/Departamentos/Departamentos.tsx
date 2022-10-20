e1db43c5ae45bcc21f833b7c6eab2e85304b2d32
gcantre <gcantu@cecapmex.com>
2022-10-20 16:21:38 -0500

    ajuste

--- src/app/views/components/menu/catalogos/Departamentos/Departamentos.tsx
+++ src/app/views/components/menu/catalogos/Departamentos/Departamentos.tsx
@@ -1,34 +1,53 @@
 import { useEffect, useState } from 'react'
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, TextField } from '@mui/material'
+import { Box, IconButton } from '@mui/material'
 import { GridColDef } from '@mui/x-data-grid'
 import { CatalogosServices } from '../../../../../services/catalogosServices'
 import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import AddIcon from '@mui/icons-material/Add';
 import MUIXDataGrid from '../../../MUIXDataGrid'
 import { getUser } from '../../../../../services/localStorage';
 import { RESPONSE } from '../../../../../interfaces/user/UserInfo';
+import { messages } from '../../../../styles';
+import Swal from 'sweetalert2';
+import { Alert } from '../../../../../helpers/Alert';
+import { Toast } from '../../../../../helpers/Toast';
+import { DepartamentosModal } from './DepartamentosModal';
+import ButtonsAdd from '../Utilerias/ButtonsAdd';
 
 export const Departamentos = () => {
 
-
-
-  const [conDepartamentos, setDepartamentos] = useState([]);
+  const [departamento, setDepartamento] = useState([]);
+  const [modo, setModo] = useState("");
   const [open, setOpen] = useState(false);
+  const [tipoOperacion, setTipoOperacion] = useState(0);
+  const [vrows, setVrows] = useState({});
   const user: RESPONSE = JSON.parse(String(getUser()));
 
 
   const columns: GridColDef[] = [
-    { field: "NombreCorto", headerName: "Nombre Corto", width: 100 },
-    { field: "Descripcion", headerName: "Descripcion", width: 600 },
+    {
+      field: "id",
+      hide: true,
+      headerName: "Identificador",
+      width: 150,
+      description: messages.dataTableColum.id,
+    },
+    { field: "NombreCorto", headerName: "Nomenclatura", width: 120 },
+    { field: "Descripcion", headerName: "Descripción", width: 150 },
     { field: "Responsable", headerName: "Responsable", width: 150 },
     {
-      field: "acciones", headerName: "Acciones", description: "Campo de Acciones", sortable: false, width: 200, renderCell: (v) => {
+      field: "acciones",
+      headerName: "Acciones",
+      description: "Campo de Acciones",
+      sortable: false,
+      width: 150,
+      renderCell: (v) => {
         return (
           <Box>
-
-            <ModeEditOutlineIcon />
-            <IconButton >
+            <IconButton onClick={() => handleEdit(v)}>
+              <ModeEditOutlineIcon />
+            </IconButton>
+            <IconButton onClick={() => handleDelete(v)}>
               <DeleteForeverIcon />
             </IconButton>
           </Box>
@@ -38,87 +57,111 @@ export const Departamentos = () => {
 
   ];
 
+  const handleClose = () => {
+    setOpen(false);
+    consulta({ NUMOPERACION: 4 });
+  };
+
   const handleOpen = (v: any) => {
-    //setSelectedId(v.row.lastName);
-
+    setTipoOperacion(1);
+    setModo("Agregar Registro");
     setOpen(true);
+    setVrows("");
   };
 
-  const handleClose = () => setOpen(false);
-
-  const ButtonAdd = () => {
-    return (
-      <Box>
-        <IconButton color="primary" aria-label="upload picture" component="label" onClick={() => handleOpen(1)}>
-          <AddIcon />
-        </IconButton>
-      </Box>
-    );
-  }
-
-
-  const DetailsModal = () => {
-    return (
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We
-            will send updates occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
-        </DialogActions>
-      </Dialog>
-    );
+  const handleEdit = (v: any) => {
+    console.log(v);
+    setTipoOperacion(2);
+    setModo("Editar Registro");
+    setOpen(true);
+    setVrows(v);
   };
 
+  const handleDelete = (v: any) => {
+    Swal.fire({
+      icon: "info",
+      title: "Estas seguro de eliminar este registro?",
+      showDenyButton: true,
+      showCancelButton: false,
+      confirmButtonText: "Confirmar",
+      denyButtonText: `Cancelar`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        console.log(v);
+        const user: RESPONSE = JSON.parse(String(getUser()));
 
-  let data = ({
-    NUMOPERACION: 4,
-    CHID: "",
-    NUMANIO: "",
-    NUMTOTALPOB: "",
-    CHUSER: user.id
-  })
+        let data = {
+          NUMOPERACION: 3,
+          CHID: v.row.id,
+          CHUSER: user.id,
+        };
+        console.log(data);
 
+        CatalogosServices.departamentos(data).then((res) => {
+          if (res.SUCCESS) {
+            Toast.fire({
+              icon: "success",
+              title: "Registro Eliminado!",
+            });
+
+            let data = {
+              NUMOPERACION: 4,
+            };
+            consulta(data);
+          } else {
+            Alert.fire({
+              title: "Error!",
+              text: res.STRMESSAGE,
+              icon: "error",
+            });
+          }
+        });
+      } else if (result.isDenied) {
+        Swal.fire("No se realizaron cambios", "", "info");
+      }
+    });
+  };
+
+  const consulta = (data: any) => {
+    CatalogosServices.departamentos(data).then((res) => {
+      if (res.SUCCESS) {
+        Toast.fire({
+          icon: "success",
+          title: "Consulta Exitosa!",
+        });
+        console.log(data);
+        setDepartamento(res.RESPONSE);
+      } else {
+        Alert.fire({
+          title: "Error!",
+          text: res.STRMESSAGE,
+          icon: "error",
+        });
+      }
+    });
+  };
 
   useEffect(() => {
-    CatalogosServices.departamentos(data).then((res) => {
-      //  console.log(res);
-      setDepartamentos(res.RESPONSE);
-    });
+    consulta({ NUMOPERACION: 4 });
   }, []);
 
-
-
-
-
-
   return (
-
-
-    <div style={{ height: 600, width: "100%" }} >
-      <DetailsModal />
-      <ButtonAdd />
-
-      <MUIXDataGrid columns={columns} rows={conDepartamentos} />
-
-      
+    <div style={{ height: 600, width: "100%" }}>
+       {open ? (
+        <DepartamentosModal
+          open={open}
+          modo={modo}
+          tipo={tipoOperacion}
+          handleClose={handleClose}
+          dt={vrows}
+        />
+      ) : (
+        ""
+      )}
+      <ButtonsAdd handleOpen={handleOpen} />
+      <MUIXDataGrid columns={columns} rows={departamento} />
     </div>
-
-
-  )
+  );
+ 
 }
 
---
+++ src/app/views/components/menu/catalogos/Departamentos/DepartamentosModal.tsx
@@ -0,0 +1,235 @@
+import {
+  Box,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  TextField,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { Alert } from "../../../../../helpers/Alert";
+import { Toast } from "../../../../../helpers/Toast";
+import { RESPONSE } from "../../../../../interfaces/user/UserInfo";
+import { CatalogosServices } from "../../../../../services/catalogosServices";
+import { getUser } from "../../../../../services/localStorage";
+
+export const DepartamentosModal = ({
+  open,
+  modo,
+  handleClose,
+  tipo,
+  dt,
+}: {
+  open: boolean;
+  modo: string;
+  tipo: number;
+  handleClose: Function;
+  dt: any;
+}) => {
+  // CAMPOS DE LOS FORMULARIOS
+  const [id, setId] = useState("");
+  const [nombreCorto, setNombreCorto] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [responsable, setResponsable] = useState("");
+  const user: RESPONSE = JSON.parse(String(getUser()));
+
+  //IMPRESIONES DE CAMPOS
+  console.log("---------Impresión de CAMPOS------");
+  console.log("id", id);
+  console.log("nombreCorto :", nombreCorto);
+  console.log("descripcion :", descripcion);
+  console.log("responsable :", responsable);
+  console.log("---------FIN-de-Impresión de CAMPOS------");
+
+  const handleSend = () => {
+    if (!nombreCorto || !descripcion || !responsable) {
+      Alert.fire({
+        title: "Error!",
+        text: "Favor de Completar los Campos",
+        icon: "error",
+      });
+    } else {
+      let data = {
+        CHID: id,
+        CHUSER: user.id,
+        NUMOPERACION: tipo,
+        NOMBRECORTO: nombreCorto,
+        DESCRIPCION: descripcion,
+        RESPONSABLE: responsable,
+      };
+      console.log("data de modal", data);
+      handleRequest(data);
+      handleClose();
+    }
+  };
+
+  const handleRequest = (data: any) => {
+    console.log(data);
+    if (tipo == 1) {
+      //AGREGAR
+      agregar(data);
+    } else if (tipo == 2) {
+      //EDITAR
+      editar(data);
+    }
+  };
+
+  const agregar = (data: any) => {
+    CatalogosServices.departamentos(data).then((res) => {
+      if (res.SUCCESS) {
+        Toast.fire({
+          icon: "success",
+          title: "Registro Agregado!",
+        });
+        console.log("Sé pudo agregar");
+      } else {
+        Alert.fire({
+          title: "Error!",
+          text: res.STRMESSAGE,
+          icon: "error",
+        });
+        console.log("No se pudo agregar");
+      }
+    });
+  };
+
+  const editar = (data: any) => {
+    CatalogosServices.departamentos(data).then((res) => {
+      if (res.SUCCESS) {
+        Toast.fire({
+          icon: "success",
+          title: "Registro Editado!",
+        });
+      } else {
+        Alert.fire({
+          title: "Error!",
+          text: res.STRMESSAGE,
+          icon: "error",
+        });
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (dt === "") {
+    } else {
+      //SE PINTAN LOS CAMPOS
+      setId(dt?.row?.id);
+      setNombreCorto(dt?.row?.NombreCorto);
+      setDescripcion(dt?.row?.Descripcion);
+      setResponsable(dt?.row?.Responsable);
+    }
+  }, [dt]);
+
+  return (
+    <Dialog open={open}>
+      <DialogContent>
+        <Box>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <label className="Titulo">{modo}</label>
+          </Box>
+          {modo === "Agregar Registro" ? (
+            <Container maxWidth="sm">
+              <TextField
+                required
+                margin="dense"
+                id="NombreCorto"
+                label="Nomenclatura"
+                value={nombreCorto}
+                type="text"
+                fullWidth
+                variant="standard"
+                onChange={(v) => setNombreCorto(v.target.value)}
+                error={!nombreCorto ? true : false}
+                InputProps={{}}
+              />
+              <TextField
+                required
+                margin="dense"
+                id="Descripcion"
+                label="Descripción"
+                value={descripcion}
+                type="text"
+                fullWidth
+                variant="standard"
+                onChange={(v) => setDescripcion(v.target.value)}
+                error={!descripcion ? true : false}
+                InputProps={{}}
+              />
+              <TextField
+                required
+                margin="dense"
+                id="Responsable"
+                label="Responsable"
+                value={responsable}
+                type="text"
+                fullWidth
+                variant="standard"
+                onChange={(v) => setResponsable(v.target.value)}
+                error={!responsable ? true : false}
+                InputProps={{}}
+              />
+            </Container>
+          ) : (
+            ""
+          )}
+
+          {modo === "Editar Registro" ? (
+            <Container maxWidth="sm">
+              <TextField
+                disabled
+                required
+                margin="dense"
+                id="NombreCorto"
+                label="Nomenclatura"
+                value={nombreCorto}
+                type="text"
+                fullWidth
+                variant="standard"
+                onChange={(v) => setNombreCorto(v.target.value)}
+                error={!nombreCorto ? true : false}
+                InputProps={{}}
+              />
+              <TextField
+                required
+                margin="dense"
+                id="Descripcion"
+                label="Descripción"
+                value={descripcion}
+                type="text"
+                fullWidth
+                variant="standard"
+                onChange={(v) => setDescripcion(v.target.value)}
+                error={!descripcion ? true : false}
+                InputProps={{}}
+              />
+              <TextField
+                required
+                margin="dense"
+                id="Responsable"
+                label="Responsable"
+                value={responsable}
+                type="text"
+                fullWidth
+                variant="standard"
+                onChange={(v) => setResponsable(v.target.value)}
+                error={!responsable ? true : false}
+                InputProps={{}}
+              />
+            </Container>
+          ) : (
+            ""
+          )}
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <button className="guardar" onClick={() => handleSend()}>
+          Guardar
+        </button>
+        <button className="cerrar" onClick={() => handleClose()}>
+          Cerrar
+        </button>
+      </DialogActions>
+    </Dialog>
+  );
+};
