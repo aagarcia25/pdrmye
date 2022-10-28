9db7757d297b8ba7b599c8f3b5a4978ab35722e5
Juanita Reyes <sjj.reyes24@gmail.com>
2022-10-28 12:55:13 -0500

    actualización

--- src/app/environments/env.ts
+++ src/app/environments/env.ts
@@ -4,11 +4,11 @@ const env = {
       BASE_URL_EXT: 'http://10.200.4.164:5000/api/'
     },
     test: {
-      BASE_URL: 'http://10.200.4.165:3002/api/Api_AdSisUs/',
+      BASE_URL: 'http://10.200.4.164:80/api/Api_AdSisUs/',
       BASE_URL_EXT: 'http://10.200.4.164:5000/api/'
     },
     production: {
-      BASE_URL: 'http://10.200.4.165:3002/api/Api_AdSisUs/',
+      BASE_URL: 'http://10.200.4.164:80/api/Api_AdSisUs/',
       BASE_URL_EXT: 'http://10.200.4.164:5000/api/'
     },
   };
