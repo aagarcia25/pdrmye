0584f51d4414739364fa4d795bafdae74cc9de23
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2022-10-28 16:20:57 -0500

    Update env.ts

--- src/app/environments/env.ts
+++ src/app/environments/env.ts
@@ -1,15 +1,15 @@
 const env = {
   development: {
       BASE_URL: 'http://10.200.4.164:80/api/Api_AdSisUs/',
-      BASE_URL_EXT: 'http://10.200.4.105:5000/api/'
+      BASE_URL_EXT: 'http://10.200.4.164:5000/api/'
     },
     test: {
       BASE_URL: 'http://10.200.4.164:80/api/Api_AdSisUs/',
-      BASE_URL_EXT: 'http://10.200.4.105:5000/api/'
+      BASE_URL_EXT: 'http://10.200.4.164:5000/api/'
     },
     production: {
       BASE_URL: 'http://10.200.4.164:80/api/Api_AdSisUs/',
-      BASE_URL_EXT: 'http://10.200.4.105:5000/api/'
+      BASE_URL_EXT: 'http://10.200.4.164:5000/api/'
     },
   };
   
