3b67837f946dbfaa16d4d9ede20e98363abdc7a3
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2022-11-04 14:59:56 -0600

    Update env.ts
    se ajustan valores

--- src/app/environments/env.ts
+++ src/app/environments/env.ts
@@ -1,11 +1,28 @@
 const env = {
+
   
+  development: {
+      BASE_URL: 'http://10.200.4.105:80/api/Api_AdSisUs/',
+      BASE_URL_EXT: 'http://10.200.4.105:5000/api/',
+      BASE_URL_LOGIN: 'http://10.200.4.106/'
+    },
+
     test: {
       BASE_URL: 'http://10.200.4.164:80/api/Api_AdSisUs/',
       BASE_URL_EXT: 'http://10.200.4.164:5000/api/',
       BASE_URL_LOGIN: 'http://10.200.4.105:5000/api/'
     },
-  
+    
+
+    production: {
+      BASE_URL: 'http://10.200.4.164:80/api/Api_AdSisUs/',
+      BASE_URL_EXT: 'http://10.200.4.164:5000/api/',
+      BASE_URL_LOGIN: 'http://10.200.4.105:5000/api/'
+    },
+   
+
   };
-  
+
+ 
+
   export const env_var = env[process.env.NODE_ENV];
