619ea3e18e5af74fce62b5938a5324fa73e5bd13
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2022-11-04 13:00:58 -0600

    Configuracion
    se Configura para despliegue en diversos ambientes

--- src/app/environments/env.ts
+++ src/app/environments/env.ts
@@ -1,5 +1,11 @@
 const env = {
 
+  local: {
+    BASE_URL: 'http://10.200.4.96:81/api/Api_AdSisUs/',
+    BASE_URL_EXT: 'http://10.200.4.105:5000/api/',
+    BASE_URL_LOGIN: 'http://10.200.4.106/'
+  },
+
   development: {
       BASE_URL: 'http://10.200.4.96:81/api/Api_AdSisUs/',
       BASE_URL_EXT: 'http://10.200.4.105:5000/api/',
