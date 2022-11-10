82b0784116de43cbb37c5f00fc7f0caf3ded69d3
abelinoolguin0402 <112125177+abelinoolguin0402@users.noreply.github.com>
2022-11-10 10:32:54 -0600

    Update FondosModal.tsx

--- src/app/views/components/menu/catalogos/Fondos/FondosModal.tsx
+++ src/app/views/components/menu/catalogos/Fondos/FondosModal.tsx
@@ -308,10 +308,22 @@ const FondosModal = ({
             </Grid>
           </Grid>
         </DialogContent>
-        <DialogActions>
-          <Button className={tipo==1?"guardar":"actualizar"} onClick={() => handleSend()}>{tipo==1?"Guardar":"Actualizar"}</Button>
-          {/* <button className="cerrar" onClick={() => handleClose()}>Cancelar</button> */}
-        </DialogActions>
+         <Grid container
+          sx={{
+            mt: "2vh",
+            width: "100%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "row",
+          }}
+        >
+          <Grid item xs={4} sm={3} md={2} lg={1}
+          >
+            <Button className={tipo==1?"guardar":"actualizar"} onClick={() => handleSend()}>{tipo==1?"Guardar":"Actualizar"}</Button>
+          </Grid>
+        </Grid>
+
 
       </ModalForm>
     </div>
