8a00c5f5c217062b73f596a56ac531c299d02735
abelinoolguin0402 <112125177+abelinoolguin0402@users.noreply.github.com>
2022-11-24 10:29:06 -0600

    Update env.ts

--- src/app/environments/env.ts
+++ src/app/environments/env.ts
@@ -2,9 +2,9 @@ const env = {
 
   
   development: {
-    BASE_URL: 'http://10.200.4.96:81/api/Api_AdSisUs/',
-    BASE_URL_EXT: 'http://10.200.4.105:5000/api/',
-    BASE_URL_LOGIN: 'http://10.200.4.106/'
+    BASE_URL: 'http://10.200.4.164:80/api/Api_AdSisUs/',
+    BASE_URL_EXT: 'http://10.200.4.164:5000/api/',
+    BASE_URL_LOGIN: 'http://10.200.4.165/'
     },
 
     test: {
