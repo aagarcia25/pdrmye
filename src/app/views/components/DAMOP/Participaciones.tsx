0105aec00932ea0fbcb478d6db718b3b288ec3d7
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2022-12-05 16:13:42 -0600

    Update Participaciones.tsx

--- src/app/views/components/DAMOP/Participaciones.tsx
+++ src/app/views/components/DAMOP/Participaciones.tsx
@@ -107,7 +107,13 @@ const Participaciones = () => {
       description: "Municipio",
     },
     {
-      field: "Nombre",
+      field: "ClaveBeneficiario",
+      headerName: "Cve. Beneficiario",
+      width: 150,
+      description: "Beneficiario",
+    },
+    {
+      field: "DescripcionBeneficiario",
       headerName: "Beneficiario",
       width: 150,
       description: "Beneficiario",
@@ -137,6 +143,11 @@ const Participaciones = () => {
       field: "ConceptoEgreso",
       headerName: "Cpto. de  egreso",
       width: 150,
+    },
+    {
+      field: "conceptoCheque",
+      headerName: "Cpto. de  Cheque",
+      width: 270,
     },
     {
       field: "tipocalculo",
