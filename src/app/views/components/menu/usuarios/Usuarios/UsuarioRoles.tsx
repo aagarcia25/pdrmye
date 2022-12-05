61f9bf9a4194c0761e126f6791127e820ea482c2
abelinoolguin0402 <112125177+abelinoolguin0402@users.noreply.github.com>
2022-12-05 16:01:25 -0600

    ajustes
    ajustes

--- src/app/views/components/menu/usuarios/Usuarios/UsuarioRoles.tsx
+++ src/app/views/components/menu/usuarios/Usuarios/UsuarioRoles.tsx
@@ -31,7 +31,7 @@ const UsuarioRoles = ({
 }) => {
 
 
-    
+
   const [openSlider, setOpenSlider] = useState<boolean>(true);
   const [idRol, setIdRol] = useState<SelectValues[]>([]);
   const [roles, setRoles] = useState<[]>([]);
@@ -72,10 +72,10 @@ const UsuarioRoles = ({
   };
 
   const handleDel = (v: any) => {
-    let data = { 
+    let data = {
       TIPO: 2,
-      IDUSUARIO:dt?.id,
-      IDROL:v.id
+      IDUSUARIO: dt?.id,
+      IDROL: v.id
     };
     //console.log(data);
     AuthService.RelacionarUsuarioRol(data).then((res) => {
@@ -92,16 +92,16 @@ const UsuarioRoles = ({
 
   const handleMunicipios = () => {
     let data = {
-        TIPO:1,
-        OBJS: idRol,
-        IDUSUARIO:dt.id
+      TIPO: 1,
+      OBJS: idRol,
+      IDUSUARIO: dt.id
     };
     //console.log(data);
     setOpenSlider(true);
     AuthService.RelacionarUsuarioRol(data).then((res) => {
-       //console.log(res.RESPONSE);
-       setOpenSlider(false);
-       consulta();
+      //console.log(res.RESPONSE);
+      setOpenSlider(false);
+      consulta();
     });
   };
 
@@ -116,7 +116,7 @@ const UsuarioRoles = ({
     AuthService.usuarioRol(data).then((res) => {
       setData(res.RESPONSE);
       setOpenSlider(false);
-     
+
     });
   };
 
@@ -134,90 +134,56 @@ const UsuarioRoles = ({
   return (
     <div>
       <Slider open={openSlider}></Slider>
-      <Dialog open={open }
-        fullScreen
-        
-        >
-          <ModalForm title={"Roles Relacionados al Usuario"} handleClose={handleClose}> 
 
-        {/* <Grid container spacing={1} sx={{ bgcolor:"#CCCCCC", paddingTop:"1%", paddingBottom:"1%" }}>
-        <Grid item xs={11} sm={11} md={11} lg={11}>
-          <Box sx={{ display:"flex",justifyContent:"center", }}>
-          <Typography variant='h4'> Roles Relacionados al Usuario </Typography>
-          </Box>
-       
-        </Grid>
-        <Grid item xs={1} sm={1} md={1} lg={1} >
-        <Button variant="outlined"  onClick={() => handleClose()} >
-        <IconButton
-          aria-label="close"
-          color="error"
-          onClick={() => handleClose()}>
-          <CloseIcon />
-        </IconButton>
-        </Button>
-       
-        </Grid>
-         </Grid> */}
+      <ModalForm title={"Roles Relacionados al Usuario"} handleClose={handleClose}>
 
         <DialogContent dividers={true} >
           <Box boxShadow={3}>
-          <Grid container spacing={1} sx={{ padding:"1%"}}>
+            <Grid container spacing={1} sx={{ padding: "1%" }}>
 
-            <Grid item xs={12} sm={12} md={12} lg={12}>
-              <Box
-                sx={{
-                  margin: 0,
-                }}
-              >
+              <Grid item xs={12} sm={12} md={12} lg={12}>
+                <Box sx={{ margin: 0, }}>
+                  <Typography sx={{ fontFamily: "sans-serif" }}>
+                    Seleccione Rol
+                  </Typography>
+
+                  <SelectFragMulti
+                    options={roles}
+                    onInputChange={handleFilterChange1}
+                    placeholder={"Seleccione Rol"}
+                    label={""}
+                    disabled={false}
+                  />
+                </Box>
+              </Grid>
+
+              <Grid item xs={12} sm={12} md={12} lg={12}>
+                <Button
+                  color="success"
+                  variant="contained"
+                  onClick={() => handleMunicipios()}
+                >
+                  <Typography sx={{ fontFamily: "sans-serif", color: "white" }}>
+                    Relacionar Roles
+                  </Typography>
+                </Button>
+              </Grid>
+
+              <Grid item xs={12} sm={12} md={12} lg={12}>
                 <Typography sx={{ fontFamily: "sans-serif" }}>
-                  Seleccione Rol
+                  Roles Relacionados
                 </Typography>
-
-                <SelectFragMulti
-                  options={roles}
-                  onInputChange={handleFilterChange1}
-                  placeholder={"Seleccione Rol"}
-                  label={""}
-                  disabled={false}
-                />
-              </Box>
+              </Grid>
+              <Grid item xs={12} sm={12} md={12} lg={12} >
+                <div style={{ height: 300, width: "100%" }}>
+                  <MUIXDataGridSimple columns={columns} rows={data} />
+                </div>
+              </Grid>
             </Grid>
-
-            <Grid item xs={12} sm={12} md={12} lg={12}>
-              <Button
-                color="success"
-                variant="contained"
-                onClick={() =>handleMunicipios()}
-              >
-                <Typography sx={{ fontFamily: "sans-serif", color:"white" }}>
-                  Relacionar Roles
-                </Typography>
-              </Button>
-            </Grid>
-
-            <Grid item xs={12} sm={12} md={12} lg={12}>
-              <Typography sx={{ fontFamily: "sans-serif" }}>
-                Roles Relacionados
-              </Typography>
-            </Grid>
-            <Grid item xs={12} sm={12} md={12} lg={12} >
-            <div style={{ height: 300, width: "100%" }}>
-              <MUIXDataGridSimple columns={columns} rows={data} />
-              </div>
-            </Grid>
-          </Grid>
           </Box>
         </DialogContent>
+      </ModalForm>
 
-        </ModalForm>
-
-        {/* <DialogActions>
-          <button className="cerrar" onClick={() => handleClose()}>
-            Cerrar
-          </button>
-        </DialogActions> */}
-      </Dialog>
     </div>
   );
 };
