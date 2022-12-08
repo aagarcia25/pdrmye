f194f5f24130bd5463cc6f894822c906798ea283
abelinoolguin0402 <112125177+abelinoolguin0402@users.noreply.github.com>
2022-12-08 12:57:43 -0600

    ajustes en carrousel

--- src/app/views/components/Bienvenido.tsx
+++ src/app/views/components/Bienvenido.tsx
@@ -1,11 +1,14 @@
- import React from "react";
- import { Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Box, Grid } from "@mui/material";
 import PlantillaBienvenido from "./PlantillaBienvenido";
 import { Carousel } from 'antd';
-import { Box } from "@mui/system";
 import imga1 from '../../assets/img/imagen1.jpg'
 import imga2 from '../../assets/img/imagen2.png'
 import imga3 from '../../assets/img/imagen3.png'
+import { CatalogosServices } from "../../services/catalogosServices";
+import { Toast } from "../../helpers/Toast";
+import { AlertS } from "../../helpers/AlertS";
+import { imagen } from "../../interfaces/user/User";
 
 // import "@fontsource/poppins"; 
 
@@ -16,104 +19,138 @@ const img3 = imga3;
 const img4 = imga2;
 
 const contentStyle: React.CSSProperties = {
-  height:"65vh",
-  // color: '#fff',
-  // lineHeight: '160px',
-  // background: '#808080',
+  height: "65vh",
   alignContent: "center",
-  margin:"1%",
+  margin: "1%",
 };
 
 
-const CarouselAp: React.FC = () => (
-  <Carousel autoplay>
 
-    <div>
-    <Box style={contentStyle} display="flex" justifyContent="center" > 
-    <Box
-        boxShadow={3}
-        component="img"
-        style={{ objectFit: "scale-down", }}
-        sx={{
-          height: "100%",
-          width: "100%",
-          maxHeight:{xs:"40%", md:"100%"},
-          background: '#08c4b3',
-        }}
-        alt="NUEVO LEÓN"
-        src={img1}
-      />
-    </Box>
-    </div>
-
-    <div>
-    <Box style={contentStyle} display="flex" justifyContent="center" >
-    <Box
-         boxShadow={3}
-        component="img"
-        style={{ objectFit: "scale-down", }}
-        sx={{
-          height: "100%",
-          width: "100%",
-          maxHeight:{xs:"40%", md:"100%"},
-          background: '#05a7e8',                                                                                                                                                                 
-        }}
-        alt="NUEVO LEÓN"
-        src={img2}
-      />
-    </Box>
-    </div>
-
-    <div>
-    <Box style={contentStyle} display="flex" justifyContent="center"> 
-    <Box
-        boxShadow={3}
-        component="img"
-        style={{ objectFit: "scale-down", }}
-        sx={{
-          height: "100%",
-          width: "100%",
-          maxHeight:{xs:"40%", md:"100%"},
-          background: '#08c4b3', 
-        }}
-        alt="NUEVO LEÓN"
-        src={img3}
-      />
-    </Box>
-    </div>
-
-    <div>
-    <Box style={contentStyle} display="flex" justifyContent="center" > 
-    <Box
-        boxShadow={3}
-        component="img"
-        style={{ objectFit: "scale-down", }}
-        sx={{
-          height: "100%",
-          width: "100%",
-          maxHeight:{xs:"40%", md:"100%"},
-          background: '#05a7e8', 
-        }}
-        alt="NUEVO LEÓN"
-        src={img4}
-      />
-    </Box>
-    </div>
-    
-
-  </Carousel>
-);
 
 export default function Bienvenido({ user }: { user: any }) {
 
-  return (
-      <Grid padding={1}>
-        <Grid item>
+  const [imagen, setImagenes] = useState<Array<imagen>>([]);
+  const consulta = (data: any) => {
+    CatalogosServices.eventos(data).then((res) => {
+      if (res.SUCCESS) {
+        Toast.fire({
+          icon: "success",
+          title: "Consulta Exitosa!",
+        });
+        setImagenes(res.RESPONSE);
+      } else {
+        AlertS.fire({
+          title: "Error!",
+          text: res.STRMESSAGE,
+          icon: "error",
+        });
+      }
+    });
+  };
 
-        </Grid>
-        <CarouselAp/>
-        <Box > </Box>
+  const CarouselAp: React.FC = () => (
+
+    <Carousel autoplay dotPosition={"top"}>
+      {
+        imagen.map((item: imagen) => {
+          return (
+            <Box style={contentStyle} display="flex" justifyContent="center" >
+              <Box
+                boxShadow={3}
+                component="img"
+                style={{ objectFit: "scale-down", }}
+                sx={{
+                  height: "100%",
+                  width: "100%",
+                  maxHeight: { xs: "40%", md: "100%" },
+                  background: '#FFFFFF',
+                }}
+                alt="NUEVO LEÓN"
+                src={item.Imagen}
+              />
+            </Box>
+
+          );
+
+        })
+
+      }
+
+      {/* <div>
+          <Box style={contentStyle} display="flex" justifyContent="center" >
+            <Box
+              boxShadow={3}
+              component="img"
+              style={{ objectFit: "scale-down", }}
+              sx={{
+                height: "100%",
+                width: "100%",
+                maxHeight: { xs: "40%", md: "100%" },
+                background: '#05a7e8',
+              }}
+              alt="NUEVO LEÓN"
+              src={img2}
+            />
+          </Box>
+        </div>
+    
+        <div>
+          <Box style={contentStyle} display="flex" justifyContent="center">
+            <Box
+              boxShadow={3}
+              component="img"
+              style={{ objectFit: "scale-down", }}
+              sx={{
+                height: "100%",
+                width: "100%",
+                maxHeight: { xs: "40%", md: "100%" },
+                background: '#08c4b3',
+              }}
+              alt="NUEVO LEÓN"
+              src={img3}
+            />
+          </Box>
+        </div> */}
+
+      {/* <div>
+        <Box style={contentStyle} display="flex" justifyContent="center" > 
+        <Box
+            boxShadow={3}
+            component="img"
+            style={{ objectFit: "scale-down", }}
+            sx={{
+              height: "100%",
+              width: "100%",
+              maxHeight:{xs:"40%", md:"100%"},
+              background: '#05a7e8', 
+            }}
+            alt="NUEVO LEÓN"
+            src={img4}
+          />
+        </Box>
+        </div> */}
+
+
+    </Carousel>
+  );
+
+  useEffect(() => {
+
+    consulta({
+      NUMOPERACION: 5,
+      CHUSER: user.id
+    });
+  }, []);
+
+
+  return (
+    <Grid padding={1}>
+      <Grid item>
+
       </Grid>
-     
+      <CarouselAp />
+      <Box > </Box>
+    </Grid>
+
   );
 }
