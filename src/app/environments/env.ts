40bd9fc51e775a13011adaeb096f1a489207af80
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2022-12-15 17:42:01 -0600

    ajustes

--- src/app/environments/env.ts
+++ src/app/environments/env.ts
@@ -8,7 +8,7 @@ const env = {
     },
 
     test: {
-      BASE_URL: 'http://10.200.4.105:80/api/Api_AdSisUs/',
+    BASE_URL: 'http://10.200.4.105:80/api/Api_AdSisUs/',
     BASE_URL_EXT: 'http://10.200.4.105:5000/api/',
     BASE_URL_LOGIN: 'http://10.200.4.106/'
     },
