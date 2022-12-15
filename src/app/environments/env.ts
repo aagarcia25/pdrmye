1c68ea9aacf4232c307c34300c4928f53f28af64
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2022-12-15 17:17:40 -0600

    ajustes
    ajustes

--- src/app/environments/env.ts
+++ src/app/environments/env.ts
@@ -2,22 +2,22 @@ const env = {
 
   
   development: {
-    BASE_URL: 'http://10.200.4.96:81/api/Api_AdSisUs/',
+    BASE_URL: 'http://10.200.4.105:80/api/Api_AdSisUs/',
     BASE_URL_EXT: 'http://10.200.4.105:5000/api/',
     BASE_URL_LOGIN: 'http://10.200.4.106/'
     },
 
     test: {
-      BASE_URL: 'http://10.200.4.164:80/api/Api_AdSisUs/',
-      BASE_URL_EXT: 'http://10.200.4.164:5000/api/',
-      BASE_URL_LOGIN: 'http://10.200.4.165/'
+      BASE_URL: 'http://10.200.4.105:80/api/Api_AdSisUs/',
+      BASE_URL_EXT: 'http://10.200.4.105:5000/api/',
+      BASE_URL_LOGIN: 'http://10.200.4.106/'
     },
     
 
     production: {
-      BASE_URL: 'http://10.200.4.164:80/api/Api_AdSisUs/',
-      BASE_URL_EXT: 'http://10.200.4.164:5000/api/',
-      BASE_URL_LOGIN: 'http://10.200.4.165/'
+      BASE_URL: 'http://10.200.4.105:80/api/Api_AdSisUs/',
+      BASE_URL_EXT: 'http://10.200.4.105:5000/api/',
+      BASE_URL_LOGIN: 'http://10.200.4.106/'
     },
    
 
--- src/app/views/components/DPCP/AuthSolicitudes.tsx
+++ src/app/views/components/DPCP/AuthSolicitudes.tsx
@@ -1,36 +1,23 @@
 import {
-  Box,
   Button,
   createTheme,
   Grid,
-  IconButton,
-  Link,
   ThemeProvider,
-  ToggleButton,
-  ToggleButtonGroup,
-  Tooltip,
   Typography,
 } from "@mui/material";
+import clsx from 'clsx';
 import React, { useEffect, useState } from "react";
 import SelectValues from "../../../interfaces/Select/SelectValues";
 import { CatalogosServices } from "../../../services/catalogosServices";
 import SelectFrag from "../Fragmentos/SelectFrag";
 import SendIcon from "@mui/icons-material/Send";
-import SettingsSuggestIcon from "@mui/icons-material/SettingsSuggest";
 import { AlertS } from "../../../helpers/AlertS";
 import { Moneda } from "../menu/CustomToolbar";
-import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 import { PERMISO, RESPONSE } from "../../../interfaces/user/UserInfo";
 import { getPermisos, getUser } from "../../../services/localStorage";
 import { DPCPServices } from "../../../services/DPCPServices";
 import { Toast } from "../../../helpers/Toast";
 import Slider from "../Slider";
-import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
-import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
-import LanIcon from "@mui/icons-material/Lan";
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
-import AccountTreeIcon from "@mui/icons-material/AccountTree";
-
 import {
   DataGrid,
   GridSelectionModel,
@@ -38,17 +25,12 @@ import {
   esES as gridEsES,
 } from "@mui/x-data-grid";
 import { esES as coreEsES } from "@mui/material/locale";
-import Swal from "sweetalert2";
-import ModalCalculos from "../componentes/ModalCalculos";
-import { DAMOPServices } from "../../../services/DAMOPServices";
-import ModalDAMOP from "../componentes/ModalDAMOP";
-import MUIXDataGridMun from "../MUIXDataGridMun";
+
 
 const AuthSolicitudes = () => {
   const theme = createTheme(coreEsES, gridEsES);
   const [slideropen, setslideropen] = useState(true);
   //MODAL
-  const [openModal, setOpenModal] = useState<boolean>(false);
   //Constantes para llenar los select
   const [selectionModel, setSelectionModel] = React.useState<GridSelectionModel>([]);
   const [fondos, setFondos] = useState<SelectValues[]>([]);
@@ -57,42 +39,29 @@ const AuthSolicitudes = () => {
   const [checkboxSelection, setCheckboxSelection] = useState(true);
   const [vrows, setVrows] = useState<{}>("");
   //Constantes de los filtros
-  const [numerooperacion, setnumerooperacion] = useState(0);
   const [idtipo, setIdTipo] = useState("");
   const [idFondo, setIdFondo] = useState("");
   const [idMunicipio, setidMunicipio] = useState("");
   //Constantes para las columnas
   const [data, setData] = useState([]);
   const user: RESPONSE = JSON.parse(String(getUser()));
-  const [plantilla, setPlantilla] = useState("");
+
   /// Permisos
   const permisos: PERMISO[] = JSON.parse(String(getPermisos()));
-  const [cargarPlant, setCargarPlant] = useState<boolean>(false);
-  const [descPlant, setDescPlant] = useState<boolean>(false);
-  const [disFide, setDisFide] = useState<boolean>(false);
-  const [solParticipaciones, setsolParticipaciones] = useState<boolean>(false);
-
-
-
-
-  const downloadplantilla = () => {
-    let data = {
-      NUMOPERACION: "PLANTILLA CARGA ANTICIPO PARTICIPACIONES",
-    };
-
-    CatalogosServices.descargaplantilla(data).then((res) => {
-      setPlantilla(res.RESPONSE);
-    });
-  };
-
   const handleDescuento = (data: any) => { };
 
   const columnsParticipaciones = [
     { field: "id", hide: true },
     {
       field: "Anio",
+      headerName: "Numero de Solicitud",
+      width: 80,
+      description: "Ejercicio",
+    },
+    {
+      field: "Anio",
       headerName: "Ejercicio",
-      width: 100,
+      width: 80,
       description: "Ejercicio",
     },
     {
@@ -101,6 +70,17 @@ const AuthSolicitudes = () => {
       width: 100,
       description: "Mes",
     },
+    {
+      field: "uresclave",
+      headerName: "U. Resp",
+      width: 65,
+    },
+    // {
+    //   field: "TipoSolicitud",
+    //   headerName: "Tipo",
+    //   width: 140,
+    //   description: "Tipo de Solicitud",
+    // },
     {
       field: "ClaveEstado",
       headerName: "Clave Estado",
@@ -114,33 +94,9 @@ const AuthSolicitudes = () => {
       description: "Municipio",
     },
     {
-      field: "Clave",
-      headerName: "Fondo",
-      width: 150,
-      description: "Fondo",
-    },
-
-    {
-      field: "uresclave",
-      headerName: "U. Resp",
-      width: 100,
-    },
-    {
-      field: "NumProyecto",
-      headerName: "Proyecto",
-      width: 150,
-    },
-    {
-      field: "ConceptoEgreso",
-      headerName: "Cpto. de  egreso",
-      width: 150,
-    },
-    {
-      field: "total",
-      headerName: "Importe",
-      width: 150,
-      description: "Importe",
-      ...Moneda,
+      field: "fondodes",
+      headerName: "Descripción de Fondo",
+      width: 250,
     },
     {
       field: "ClavePresupuestal",
@@ -148,6 +104,47 @@ const AuthSolicitudes = () => {
       width: 600,
       hide: false,
     },
+    
+    {
+      field: "estatus",
+      headerName: "Estatus",
+      width: 150,
+    },
+    {
+      field: "Presupuesto",
+      headerName: "Presupuesto SIREGOB",
+      width: 150,
+      description: "Presupuesto SIREGOB",
+      ...Moneda,
+    },
+    {
+      field: "total",
+      headerName: "Total Neto",
+      width: 150,
+      description: "Total Neto",
+      ...Moneda,
+    },
+    /*{
+      field: "Retenciones",
+      headerName: "Retenciones",
+      width: 150,
+      description: "Retenciones",
+      ...Moneda,
+    },
+    {
+      field: "Descuentos",
+      headerName: "Descuentos",
+      width: 150,
+      description: "Descuentos",
+      ...Moneda,
+    },
+    {
+      field: "importe",
+      headerName: "Importe Total",
+      width: 150,
+      description: "Importe Total = Total Neto - (Retenciones + Descuentos)",
+      ...Moneda,
+    },*/
 
   ];
 
@@ -165,9 +162,7 @@ const AuthSolicitudes = () => {
     });
   };
 
-  const handleClose = () => {
-    setOpenModal(false);
-  };
+ 
 
   const handleFilterChange1 = (v: string) => {
     setIdTipo(v);
@@ -181,157 +176,12 @@ const AuthSolicitudes = () => {
     setidMunicipio(v);
   };
 
-  const Fnworkflow = (data: string) => {
-    let obj = {
-      NUMOPERACION: numerooperacion,
-      OBJS: selectionModel,
-      CHUSER: user.id,
-      COMENTARIO: data,
-      ESTATUS: "DPCP_INICIO",
-    };
+  
+ 
+ 
 
-    DAMOPServices.PA(obj).then((res) => {
-      if (res.SUCCESS) {
-        Toast.fire({
-          icon: "success",
-          title: "Consulta Exitosa!",
-        });
-        handleClick();
-        handleClose();
-      } else {
-        AlertS.fire({
-          title: "Error!",
-          text: res.STRMESSAGE,
-          icon: "error",
-        });
-      }
-    });
-  };
+ 
 
-  const openmodalc = (operacion: number) => {
-    if (selectionModel.length > 0) {
-      setnumerooperacion(operacion);
-      setOpenModal(true);
-    } else {
-      AlertS.fire({
-        title: "Error!",
-        text: "Favor de Seleccionar Registros",
-        icon: "error",
-      });
-    }
-  };
-
-  const handleUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setslideropen(true);
-    let file = event?.target?.files?.[0] || "";
-    const formData = new FormData();
-    formData.append("inputfile", file, "inputfile.xlxs");
-    formData.append("CHUSER", user.id);
-    formData.append("tipo", "anticipoParticipaciones");
-    CatalogosServices.migraData(formData).then((res) => {
-      setslideropen(false);
-      handleClick();
-    });
-  };
-
-  const handleBorrar = () => {
-  };
-
-  const Disitribuir = () => {
-    if (selectionModel.length === 1) {
-      Swal.fire({
-        icon: "info",
-        title: "Distribucion",
-        text: "El Movimiento Seleccionado se distribura entre los Fideicomisos Relacionados al Municipio",
-        showDenyButton: false,
-        showCancelButton: true,
-        confirmButtonText: "Aceptar",
-        cancelButtonText: "Cancelar",
-      }).then(async (result) => {
-        if (result.isConfirmed) {
-          let data = {
-            OBJS: selectionModel,
-            CHUSER: user.id,
-          };
-
-          AlertS.fire({
-            title: "Solicitud Enviada",
-            icon: "success",
-          }).then(async (result) => {
-            if (result.isConfirmed) {
-              DPCPServices.DistribucionFideicomisos(data).then((res) => {
-                if (res.SUCCESS) {
-                  Toast.fire({
-                    icon: "success",
-                    title: "Consulta Exitosa!",
-                  });
-                  handleClick();
-                } else {
-                  AlertS.fire({
-                    title: "Error!",
-                    text: res.STRMESSAGE,
-                    icon: "error",
-                  });
-                }
-              });
-            }
-          });
-
-
-
-        }
-      });
-    } else if (selectionModel.length > 1) {
-      AlertS.fire({
-        title: "Error!",
-        text: "Solo se permite seleccionar un registro para La distribución",
-        icon: "error",
-      });
-    } else {
-      AlertS.fire({
-        title: "Error!",
-        text: "Favor de Seleccionar Registros",
-        icon: "error",
-      });
-    }
-  };
-
-  const SolicitudOrdenPago = () => {
-    if (selectionModel.length === 0) {
-      AlertS.fire({
-        title: "Error!",
-        text: "Favor de Seleccionar Registros",
-        icon: "error",
-      });
-    } else {
-      Swal.fire({
-        icon: "warning",
-        title: "Solicitar",
-        text: selectionModel.length + " Elementos Seleccionados",
-        showDenyButton: false,
-        showCancelButton: true,
-        confirmButtonText: "Aceptar",
-        cancelButtonText: "Cancelar",
-      }).then(async (result) => {
-        if (result.isConfirmed) {
-          let data = {
-            TIPO: 1,
-            OBJS: selectionModel,
-            CHUSER: user.id,
-          };
-
-          AlertS.fire({
-            title: "Solicitud Enviada",
-            icon: "success",
-          }).then(async (result) => {
-            if (result.isConfirmed) {
-              handleClick();
-            }
-          });
-        }
-      });
-    }
-  };
 
   const handleClick = () => {
     //console.log("EJECUTANDO LA CONSULTA CON LOS SIGUIENTES FILTROS");
@@ -343,7 +193,6 @@ const AuthSolicitudes = () => {
     };
     //console.log(data);
     DPCPServices.GetParticipaciones(data).then((res) => {
-      console.log(res.RESPONSE)
       if (res.SUCCESS) {
         Toast.fire({
           icon: "success",
@@ -364,74 +213,50 @@ const AuthSolicitudes = () => {
     loadFilter(12);
     loadFilter(5);
     loadFilter(17);
-    //handleClick();
-    downloadplantilla();
-    permisos.map((item: PERMISO) => {
+    handleClick();
+  /*  permisos.map((item: PERMISO) => {
       if (
-        String(item.ControlInterno) === "DPCPAUTHSOL"
+        String(item.ControlInterno) === "PARTMUN"
       ) {
         //console.log(item);
-        if (String(item.Referencia) === "ADMINSOL") {
-          setsolParticipaciones(true);
+        if (String(item.Referencia) === "AGREGPLANT") {
+          setCargarPlant(true);
         }
-       
+        else if (String(item.Referencia) === "DESCPLANT") {
+          setDescPlant(true);
+        }
+        else if (String(item.Referencia) === "DISFIDE") {
+          setDisFide(true);
+        }
       }
-    });
+    });*/
   }, []);
 
   return (
     <div>
       <Slider open={slideropen}></Slider>
 
-      {openModal ? (
-        <ModalDAMOP
-          tipo={"Comentarios"}
-          handleClose={handleClose}
-          handleAccion={Fnworkflow} />
-      ) : (
-        ""
-      )}
+     
+
+
+
+
+
+    
 
       <Grid container spacing={1} padding={2}>
         <Grid container spacing={1} item xs={12} sm={12} md={12} lg={12}>
           <Grid container sx={{ justifyContent: "center" }}>
             <Grid item xs={10} sx={{ textAlign: "center" }}>
               <Typography variant="h4" paddingBottom={2}>
-               Módulo de Suficiencia Presupuestal
+              Módulo de Autorización de Solicitudes de Pago
               </Typography>
             </Grid>
           </Grid>
         </Grid>
 
         <Grid container spacing={1} item xs={12} sm={12} md={12} lg={12}>
-          <Grid item xs={4} sm={4} md={3.5} lg={3}>
-            <Typography sx={{ fontFamily: "MontserratMedium" }}>
-              Tipo:
-            </Typography>
-            <SelectFrag
-              value={idtipo}
-              options={tipos}
-              onInputChange={handleFilterChange1}
-              placeholder={"Seleccione Tipo"}
-              label={""}
-              disabled={false}
-            />
-          </Grid>
-          <Grid item xs={4} sm={4} md={3.5} lg={3}>
-            <Typography sx={{ fontFamily: "MontserratMedium" }}>
-              Fondo:
-            </Typography>
-            <SelectFrag
-              value={idFondo}
-              options={fondos}
-              onInputChange={handleFilterChange2}
-              placeholder={"Seleccione Fondo"}
-              label={""}
-              disabled={false}
-            />
-          </Grid>
-
-          <Grid item xs={4} sm={4} md={3.5} lg={3}>
+          <Grid item xs={2} sm={2} md={2} lg={2}>
             <Typography sx={{ fontFamily: "MontserratMedium" }}>
               Municipio:
             </Typography>
@@ -456,66 +281,9 @@ const AuthSolicitudes = () => {
             <Typography sx={{ color: "white" }}> Buscar </Typography>
           </Button>
         </Grid>
-        {solParticipaciones ?
-          <Grid item xs={12} sm={12} md={12} lg={12} paddingBottom={1}>
-            <ToggleButtonGroup>
-              <Tooltip title={"Solicitar Suficiencia Presupuestal"}>
-                <ToggleButton value="check" onClick={() => openmodalc(1)}>
-                  <AttachMoneyIcon />
-                </ToggleButton>
-              </Tooltip>
-              <Tooltip title={"Generar Solicitud"}>
-                <ToggleButton value="check" onClick={() => SolicitudOrdenPago()}>
-                  <SettingsSuggestIcon />
-                </ToggleButton>
-              </Tooltip>
-              <Tooltip title={"Asignar Comentario"}>
-                <ToggleButton value="check" onClick={() => openmodalc(2)}>
-                  <FormatAlignLeftIcon />
-                </ToggleButton>
-              </Tooltip>
-            </ToggleButtonGroup>
-          </Grid>
-          : ""}
 
-        <Grid item xs={12} sm={12} md={12} lg={12} paddingBottom={1}>
-          {descPlant ?
-            <Tooltip title="Descargar Plantilla">
-              <IconButton
-                aria-label="upload documento"
-                component="label"
-                size="large"
-              >
-                <Link href={plantilla}>
-                  <ArrowDownwardIcon />
-                </Link>
-              </IconButton>
-            </Tooltip> : ""}
-          {cargarPlant ?
-            <Tooltip title="Cargar Plantilla">
-              <IconButton
-                aria-label="upload documento"
-                component="label"
-                size="large"
-              >
-                <input
-                  hidden
-                  accept=".xlsx, .XLSX, .xls, .XLS"
-                  type="file"
-                  value=""
-                  onChange={(v) => handleUpload(v)}
-                />
-                <DriveFolderUploadIcon />
-              </IconButton>
-            </Tooltip> : ""}
-          {disFide ?
-            <Tooltip title={"Distribuir en Fideicomisos"}>
-              <IconButton value="check" onClick={() => Disitribuir()}>
-                <AccountTreeIcon />
-              </IconButton>
-            </Tooltip>
-            : ""}
-        </Grid>
+    
+
 
         <Grid item xs={12} sm={12} md={12} lg={12}>
           <div
@@ -524,9 +292,66 @@ const AuthSolicitudes = () => {
               width: "100%",
             }}
           >
-
-            <MUIXDataGridMun modulo={""} handleBorrar={handleBorrar} columns={columnsParticipaciones} rows={data} borrar={false} />
-
+            <ThemeProvider theme={theme}>
+              <DataGrid
+                columns={columnsParticipaciones}
+                rows={data}
+                density="compact"
+                rowsPerPageOptions={[10, 25, 50, 100]}
+                disableSelectionOnClick
+                disableColumnFilter
+                disableColumnSelector
+                disableDensitySelector
+                getRowHeight={() => "auto"}
+                getRowClassName={(params) =>
+                  {
+                    if (params.row.Presupuesto == null) {
+                      return '';
+                    }
+                    return clsx('super-app', {
+                      negative: params.row.Presupuesto !== params.row.total,
+                      positive: params.row.Presupuesto == params.row.total,
+                    });
+                  }
+                } 
+                components={{ Toolbar: GridToolbar }}
+                sx={{
+                  fontFamily: "Poppins,sans-serif", fontWeight: '600',
+                  '& .super-app.negative': {
+                    color: "rgb(84, 3, 3)",
+                    backgroundColor: "rgb(196, 40, 40, 0.384)",
+                  },
+                  '& .super-app.positive': {
+                    backgroundColor: 'rgb(16, 145, 80, 0.567)',
+                   
+                  },
+                }}
+                componentsProps={{
+                  toolbar: {
+                    label: "buscar",
+                    showQuickFilter: true,
+                    quickFilterProps: { debounceMs: 500 },
+                  },
+                }}
+                checkboxSelection={checkboxSelection}
+                onSelectionModelChange={(newSelectionModel: any) => {
+                  setSelectionModel(newSelectionModel);
+                }}
+                selectionModel={selectionModel}
+                localeText={{
+                  noRowsLabel: "No se ha encontrado datos.",
+                  noResultsOverlayLabel: "No se ha encontrado ningún resultado",
+                  toolbarColumns: "Columnas",
+                  toolbarExport: "Exportar",
+                  toolbarColumnsLabel: "Seleccionar columnas",
+                  toolbarFilters: "Filtros",
+                  toolbarFiltersLabel: "Ver filtros",
+                  toolbarFiltersTooltipHide: "Quitar filtros",
+                  toolbarFiltersTooltipShow: "Ver filtros",
+                  toolbarQuickFilterPlaceholder: "Buscar",
+                }}
+              />
+            </ThemeProvider>
           </div>
         </Grid>
       </Grid>
