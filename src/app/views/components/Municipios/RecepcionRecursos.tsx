4b70d996ee0b0be704b80f0896e922c45a496af6
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-01-03 17:27:59 -0600

    teste

--- src/app/views/components/Municipios/RecepcionRecursos.tsx
+++ src/app/views/components/Municipios/RecepcionRecursos.tsx
@@ -1,168 +1,381 @@
-import { useState } from 'react'
-import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import { TabContext, TabList, TabPanel } from '@mui/lab';
-import { ta } from 'date-fns/locale';
-import { Button, ButtonGroup, Grid, Typography } from '@mui/material';
-import MUIXDataGrid from '../MUIXDataGrid';
-import { GridColDef } from '@mui/x-data-grid';
-
-
+import {
+  Box,
+  Button,
+  createTheme,
+  Grid,
+  IconButton,
+  ThemeProvider,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import clsx from 'clsx';
+import React, { useEffect, useState } from "react";
+import SelectValues from "../../../interfaces/Select/SelectValues";
+import { CatalogosServices } from "../../../services/catalogosServices";
+import SelectFrag from "../Fragmentos/SelectFrag";
+import SendIcon from "@mui/icons-material/Send";
+import { AlertS } from "../../../helpers/AlertS";
+import { Moneda } from "../menu/CustomToolbar";
+import { PERMISO, RESPONSE } from "../../../interfaces/user/UserInfo";
+import { getPermisos, getUser } from "../../../services/localStorage";
+import { DPCPServices } from "../../../services/DPCPServices";
+import { Toast } from "../../../helpers/Toast";
+import Slider from "../Slider";
+import {
+  DataGrid,
+  GridSelectionModel,
+  GridToolbar,
+  esES as gridEsES,
+} from "@mui/x-data-grid";
+import { esES as coreEsES } from "@mui/material/locale";
+import FilePresentIcon from '@mui/icons-material/FilePresent';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
 const RecepcionRecursos = () => {
+  const theme = createTheme(coreEsES, gridEsES);
+  const [slideropen, setslideropen] = useState(true);
+  //MODAL
+  //Constantes para llenar los select
+  const [selectionModel, setSelectionModel] = React.useState<GridSelectionModel>([]);
+  const [fondos, setFondos] = useState<SelectValues[]>([]);
+  const [municipio, setMunicipios] = useState<SelectValues[]>([]);
+  const [tipos, setTipos] = useState<SelectValues[]>([]);
+  const [checkboxSelection, setCheckboxSelection] = useState(true);
+  const [vrows, setVrows] = useState<{}>("");
+  //Constantes de los filtros
+  const [idtipo, setIdTipo] = useState("");
+  const [idFondo, setIdFondo] = useState("");
+  const [idMunicipio, setidMunicipio] = useState("");
+  //Constantes para las columnas
+  const [data, setData] = useState([]);
+  const user: RESPONSE = JSON.parse(String(getUser()));
 
-  const columns: GridColDef[] = [
-    {
-      field: "id",
-      hide: true,
+  /// Permisos
+  const permisos: PERMISO[] = JSON.parse(String(getPermisos()));
+  const handleDescuento = (data: any) => { };
 
-    },
+  const columnsParticipaciones = [
+    { field: "id", hide: true },
     {
-      field: "acciones",  disableExport: true,
-      headerName: "Acciones",
-      description: "Campo de Acciones",
+      field: "acciones",
+      disableExport: true,
+      headerName: "Acción",
+      description: "Acción",
       sortable: false,
-      width: 200,
-      renderCell: (v) => {
+      width: 150,
+      renderCell: (v: any) => {
         return (
-          <>
-            {/* <Box>
-              {fideicomiso ? (
-                <Tooltip title={"Visualizar Fideicomisos"}>
-                  <IconButton onClick={() => handleFideicomiso(v)}>
-                    <RequestQuoteIcon />
-                  </IconButton>
-                </Tooltip>
-              ) : (
-                ""
-              )}
-              {viewCC ? (
-                <Tooltip title={"Visualizar Cuenta Bancaria"}>
-                  <IconButton onClick={() => handleCC(v)}>
-                    <AccountBalanceWalletIcon />
-                  </IconButton>
-                </Tooltip>
-              ) : (
-                ""
-              )}
-              {viewUR ? (
-                <Tooltip title={"Visualizar Usuario Responsable"}>
-                  <IconButton onClick={() => handleUR(v)}>
-                    <ManageAccountsIcon />
-                  </IconButton>
-                </Tooltip>
-              ) : (
-                ""
-              )}
-            </Box> */}
-            <Box>
-              {/* <BotonesAcciones
-                handleAccion={handleAccion}
-                row={v}
-                editar={editar}
-                eliminar={eliminar}
-              ></BotonesAcciones> */}
-            </Box>
-          </>
+          <Box>
+            
+              <Tooltip title="Subir CFDI">
+                <IconButton onClick={() => handleDescuento(v)}>
+                  <FilePresentIcon />
+                </IconButton>
+              </Tooltip>
+
+              <Tooltip title="Descargar SPEI">
+                <IconButton onClick={() => handleDescuento(v)}>
+                  <FileDownloadIcon />
+                </IconButton>
+              </Tooltip>
+          
+
+       
+          </Box>
         );
       },
     },
-    { field: "FechaCreacion", headerName: "Fechas Creación", width: 150 },
-    { field: "ClaveEstado", headerName: "Clave Estado", width: 120 },
-    { field: "Nombre", headerName: "Municipio", width: 250 },
+    {
+      field: "Anio",
+      headerName: "Ejercicio",
+      width: 80,
+      description: "Ejercicio",
+    },
+    {
+      field: "Mes",
+      headerName: "Mes",
+      width: 100,
+      description: "Mes",
+    },
+  
+    {
+      field: "ClaveEstado",
+      headerName: "Clave Estado",
+      width: 100,
+      description: "Clave Estado",
+    },
+    {
+      field: "Nombre",
+      headerName: "Municipio",
+      width: 150,
+      description: "Municipio",
+    },
+    {
+      field: "NumOrdenPago",
+      headerName: "Nº De Orden De Pago",
+      width: 200,
+      description: "Número De Orden De Pago",
+    },
+   
+    {
+      field: "NumCheque",
+      headerName: "Nº De Cheque",
+      width: 200,
+      description: "Número De Cheque",
+    },
+    {
+      field: "fondodes",
+      headerName: "Descripción de Fondo",
+      width: 250,
+    },
+    
 
-    { field: "NombreCorto", headerName: "Nombre Corto", width: 250 },
-    // { field: "OrdenSFTGNL", headerName: "Orden SFTGNL", width: 120 },
-    { field: "ClaveSIREGOB", headerName: "Clave SIREGOB", width: 120 },
-    { field: "ClaveINEGI", headerName: "Clave INEGI", width: 120 },
     {
-      field: "MAM",
-      headerName: "Área Metropolitana",
-      width: 100,
-      renderCell: (v) => {
-        return v.row.MAM === 1 ? "SI" : "NO";
-      },
+      field: "estatus",
+      headerName: "Estatus",
+      width: 150,
+    },
+   
+    {
+      field: "total",
+      headerName: "Total Neto",
+      width: 150,
+      description: "Total Neto",
+      ...Moneda,
     },
     {
-      field: "Descentralizado",
-      headerName: "Descentralizado",
-      width: 100,
-      renderCell: (v) => {
-        return v.row.Descentralizado === 1 ? "SI" : "NO";
-      },
+      field: "Retenciones",
+      headerName: "Retenciones",
+      width: 150,
+      description: "Retenciones",
+      ...Moneda,
     },
     {
-      field: "ArtF1",
-      headerName: "ARTF1",
-      width: 100,
-      renderCell: (v) => {
-        return v.row.ArtF1 === "1" ? "SI" : "NO";
-      },
+      field: "Descuentos",
+      headerName: "Descuentos",
+      width: 150,
+      description: "Descuentos",
+      ...Moneda,
     },
     {
-      field: "ArtF2",
-      headerName: "ARTF2",
-      width: 100,
-      renderCell: (v) => {
-        return v.row.ArtF2 === "1" ? "SI" : "NO";
-      },
+      field: "importe",
+      headerName: "Importe Total",
+      width: 150,
+      description: "Importe Total = Total Neto - (Retenciones + Descuentos)",
+      ...Moneda,
     },
-    {
-      field: "ArtF3",
-      headerName: "ARTF3",
-      width: 100,
-      renderCell: (v) => {
-        return v.row.ArtF3 === "1" ? "SI" : "NO";
-      },
-    },
+
   ];
 
-  const [recursoButton, setRecursoButton] = useState("PF");
-
-  const handleAjustesRel = (v: string) => {
-    setRecursoButton(v)
-
+  const loadFilter = (operacion: number) => {
+    let data = { NUMOPERACION: operacion };
+    CatalogosServices.SelectIndex(data).then((res) => {
+      if (operacion === 12) {
+        setFondos(res.RESPONSE);
+      } else if (operacion === 5) {
+        setMunicipios(res.RESPONSE);
+      } else if (operacion === 17) {
+        setTipos(res.RESPONSE);
+        setslideropen(false);
+      }
+    });
   };
+
+ 
+
+  const handleFilterChange1 = (v: string) => {
+    setIdTipo(v);
+  };
+
+  const handleFilterChange2 = (v: string) => {
+    setIdFondo(v);
+  };
+
+  const handleFilterChange3 = (v: string) => {
+    setidMunicipio(v);
+  };
+
+  
+ 
+ 
+
+ 
+
+
+  const handleClick = () => {
+    //console.log("EJECUTANDO LA CONSULTA CON LOS SIGUIENTES FILTROS");
+
+    let data = {
+      TIPO: 1,
+      P_FONDO: idFondo === "false" ? "" : idFondo,
+      P_IDMUNICIPIO: idMunicipio === "false" ? "" : idMunicipio,
+      P_IDTIPO: idtipo === "false" ? "" : idtipo,
+    };
+    //console.log(data);
+    DPCPServices.GetParticipaciones(data).then((res) => {
+      if (res.SUCCESS) {
+        Toast.fire({
+          icon: "success",
+          title: "Consulta Exitosa!",
+        });
+        setData(res.RESPONSE);
+      } else {
+        AlertS.fire({
+          title: "Error!",
+          text: res.STRMESSAGE,
+          icon: "error",
+        });
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadFilter(12);
+    loadFilter(5);
+    loadFilter(17);
+    handleClick();
+  /*  permisos.map((item: PERMISO) => {
+      if (
+        String(item.ControlInterno) === "PARTMUN"
+      ) {
+        //console.log(item);
+        if (String(item.Referencia) === "AGREGPLANT") {
+          setCargarPlant(true);
+        }
+        else if (String(item.Referencia) === "DESCPLANT") {
+          setDescPlant(true);
+        }
+        else if (String(item.Referencia) === "DISFIDE") {
+          setDisFide(true);
+        }
+      }
+    });*/
+  }, []);
 
   return (
     <div>
-      <Box sx={{ width: '100%', typography: 'body1', paddingLeft:"1%", paddingRight:"1%" }}>
-        <Grid container sm={12} sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <Slider open={slideropen}></Slider>
 
-          <ButtonGroup variant="outlined" aria-label="outlined primary button group">
-            
-            <Button className='subtitulo'  color={recursoButton == "PF" ? "secondary" : "inherit"} onClick={() => handleAjustesRel("PF")}> 
-            <Typography variant='body1'>
-             Participaciones Federales 
-             </Typography>
-             </Button>
-            <Button className='subtitulo' color={recursoButton == "PE" ? "secondary" : "inherit"} onClick={() => handleAjustesRel("PE")}>
-            <Typography variant='body1'>
-              Participaciones Estatales
-              </Typography>
-            </Button>
-            <Button className='subtitulo' color={recursoButton == "AF" ? "secondary" : "inherit"} onClick={() => handleAjustesRel("AF")}>
-            <Typography variant='body1'>
-              Aportaciones Federales
-              </Typography>
-            </Button>
-            <Button className='subtitulo' color={recursoButton == "AE" ? "secondary" : "inherit"} onClick={() => handleAjustesRel("AE")}>
-            <Typography variant='body1'>
-              Aportaciones Estatales
-              </Typography>
-            </Button>
-          </ButtonGroup>
-        </Grid>
+     
 
-        <Grid container>
-          <Grid item xs={12} textAlign="center">
-            {recursoButton}
-            <MUIXDataGrid sx={{}} columns={columns} rows={{}} />
+
+
+
+
+    
+
+      <Grid container spacing={1} padding={2}>
+        <Grid container spacing={1} item xs={12} sm={12} md={12} lg={12}>
+          <Grid container sx={{ justifyContent: "center" }}>
+            <Grid item xs={10} sx={{ textAlign: "center" }}>
+              <Typography variant="h4" paddingBottom={2}>
+              Módulo de Recepción de Recursos
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
 
-      </Box>
-    </div>
-  )
-}
+        <Grid container spacing={1} item xs={12} sm={12} md={12} lg={12}>
+          <Grid item xs={2} sm={2} md={2} lg={2}>
+            <Typography sx={{ fontFamily: "MontserratMedium" }}>
+              Municipio:
+            </Typography>
+            <SelectFrag
+              value={idMunicipio}
+              options={municipio}
+              onInputChange={handleFilterChange3}
+              placeholder={"Seleccione Municipio"}
+              label={""}
+              disabled={false}
+            />
+          </Grid>
+        </Grid>
 
-export default RecepcionRecursos
+        <Grid item xs={12} sm={12} md={12} lg={12} paddingBottom={2}>
+          <Button
+            onClick={handleClick}
+            variant="contained"
+            color="success"
+            endIcon={<SendIcon sx={{ color: "white" }} />}
+          >
+            <Typography sx={{ color: "white" }}> Buscar </Typography>
+          </Button>
+        </Grid>
+
+    
+
+
+        <Grid item xs={12} sm={12} md={12} lg={12}>
+          <div
+            style={{
+              height: "58vh",
+              width: "100%",
+            }}
+          >
+            <ThemeProvider theme={theme}>
+              <DataGrid
+                columns={columnsParticipaciones}
+                rows={data}
+                density="compact"
+                rowsPerPageOptions={[10, 25, 50, 100]}
+                disableSelectionOnClick
+                disableColumnFilter
+                disableColumnSelector
+                disableDensitySelector
+                getRowHeight={() => "auto"}
+                // getRowClassName={(params) =>
+                //   {
+                //     if (params.row.Presupuesto == null) {
+                //       return '';
+                //     }
+                //     return clsx('super-app', {
+                //       negative: params.row.Presupuesto !== params.row.total,
+                //       positive: params.row.Presupuesto == params.row.total,
+                //     });
+                //   }
+                // } 
+                components={{ Toolbar: GridToolbar }}
+                 sx={{
+                   fontFamily: "Poppins,sans-serif", fontWeight: '600',
+                //   '& .super-app.negative': {
+                //     color: "rgb(84, 3, 3)",
+                //     backgroundColor: "rgb(196, 40, 40, 0.384)",
+                //   },
+                //   '& .super-app.positive': {
+                //     backgroundColor: 'rgb(16, 145, 80, 0.567)',
+                   
+                //   },
+                 }}
+                componentsProps={{
+                  toolbar: {
+                    label: "buscar",
+                    showQuickFilter: true,
+                    quickFilterProps: { debounceMs: 500 },
+                  },
+                }}
+                checkboxSelection={checkboxSelection}
+                onSelectionModelChange={(newSelectionModel: any) => {
+                  setSelectionModel(newSelectionModel);
+                }}
+                selectionModel={selectionModel}
+                localeText={{
+                  noRowsLabel: "No se ha encontrado datos.",
+                  noResultsOverlayLabel: "No se ha encontrado ningún resultado",
+                  toolbarColumns: "Columnas",
+                  toolbarExport: "Exportar",
+                  toolbarColumnsLabel: "Seleccionar columnas",
+                  toolbarFilters: "Filtros",
+                  toolbarFiltersLabel: "Ver filtros",
+                  toolbarFiltersTooltipHide: "Quitar filtros",
+                  toolbarFiltersTooltipShow: "Ver filtros",
+                  toolbarQuickFilterPlaceholder: "Buscar",
+                }}
+              />
+            </ThemeProvider>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default RecepcionRecursos;
