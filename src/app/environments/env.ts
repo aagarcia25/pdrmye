bcfcf0f1d67a17cfbb6348885bf47f9d5a862016
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-02-07 11:49:52 -0600

    Update env.ts
    se actualizan variables solo para produccion

--- src/app/environments/env.ts
+++ src/app/environments/env.ts
@@ -2,22 +2,22 @@ const env = {
 
   
   development: {
-    BASE_URL: 'http://10.200.4.105:80/api/Api_AdSisUs/',
-    BASE_URL_EXT: 'http://10.200.4.105:5000/api/',
-    BASE_URL_LOGIN: 'http://10.200.4.106/'
+    BASE_URL: 'http://10.210.0.27:80/api/Api_AdSisUs/',
+    BASE_URL_EXT: 'http://10.210.0.27:3000/api/',
+    BASE_URL_LOGIN: 'http://10.210.0.28/'
     },
 
     test: {
-    BASE_URL: 'http://10.200.4.105:80/api/Api_AdSisUs/',
-    BASE_URL_EXT: 'http://10.200.4.105:5000/api/',
-    BASE_URL_LOGIN: 'http://10.200.4.106/'
+    BASE_URL: 'http://10.210.0.27:80/api/Api_AdSisUs/',
+    BASE_URL_EXT: 'http://10.210.0.27:3000/api/',
+    BASE_URL_LOGIN: 'http://10.210.0.28/'
     },
     
 
     production: {
-    BASE_URL: 'http://10.200.4.105:80/api/Api_AdSisUs/',
-    BASE_URL_EXT: 'http://10.200.4.105:5000/api/',
-    BASE_URL_LOGIN: 'http://10.200.4.106/'
+    BASE_URL: 'http://10.210.0.27:80/api/Api_AdSisUs/',
+    BASE_URL_EXT: 'http://10.210.0.27:3000/api/',
+    BASE_URL_LOGIN: 'http://10.210.0.28/'
     },
    
   };
