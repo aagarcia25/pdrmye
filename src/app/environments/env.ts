58bcf5cfdb3922743060ed3e47c09a9fa2bc1cff
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-02-07 12:57:32 -0600

    Update env.ts
    ajustes para produccion

--- src/app/environments/env.ts
+++ src/app/environments/env.ts
@@ -2,20 +2,20 @@ const env = {
 
   
   development: {
-    BASE_URL: 'http://10.210.0.27:81/api/Api_AdSisUs/',
+    BASE_URL: 'http://10.210.0.27:80/api/Api_AdSisUs/',
     BASE_URL_EXT: 'http://10.210.0.27:3000/api/',
     BASE_URL_LOGIN: 'http://10.210.0.28/'
     },
 
     test: {
-    BASE_URL: 'http://10.210.0.27:81/api/Api_AdSisUs/',
+    BASE_URL: 'http://10.210.0.27:80/api/Api_AdSisUs/',
     BASE_URL_EXT: 'http://10.210.0.27:3000/api/',
     BASE_URL_LOGIN: 'http://10.210.0.28/'
     },
     
 
     production: {
-    BASE_URL: 'http://10.210.0.27:81/api/Api_AdSisUs/',
+    BASE_URL: 'http://10.210.0.27:80/api/Api_AdSisUs/',
     BASE_URL_EXT: 'http://10.210.0.27:3000/api/',
     BASE_URL_LOGIN: 'http://10.210.0.28/'
     },
