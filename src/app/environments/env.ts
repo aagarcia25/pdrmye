88d1cccfe19c41d621bdced43c435c32bb586d98
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-02-15 12:26:40 -0600

    Update env.ts
    AJUSTES AMBIENTE QA

--- src/app/environments/env.ts
+++ src/app/environments/env.ts
@@ -2,21 +2,21 @@ const env = {
 
   
   development: {
-    BASE_URL: 'http://10.210.0.27:80/api/Api_AdSisUs/',
-    BASE_URL_EXT: 'http://10.210.0.27:3000/api/',
-    BASE_URL_LOGIN: 'http://10.210.0.28/'
+    BASE_URL: 'http://10.200.4.164:80/api/Api_AdSisUs/',
+    BASE_URL_EXT: 'http://10.200.4.164:5000/api/',
+    BASE_URL_LOGIN: 'http://10.200.4.164/'
     },
 
     test: {
-    BASE_URL: 'http://10.210.0.27:80/api/Api_AdSisUs/',
-    BASE_URL_EXT: 'http://10.210.0.27:3000/api/',
-    BASE_URL_LOGIN: 'http://10.210.0.28/'
+    BASE_URL: 'http://10.200.4.164:80/api/Api_AdSisUs/',
+    BASE_URL_EXT: 'http://10.200.4.164:5000/api/',
+    BASE_URL_LOGIN: 'http://10.200.4.164/'
     },
 
     production: {
-    BASE_URL: 'http://10.210.0.27:80/api/Api_AdSisUs/',
-    BASE_URL_EXT: 'http://10.210.0.27:3000/api/',
-    BASE_URL_LOGIN: 'http://10.210.0.28/'
+    BASE_URL: 'http://10.200.4.164:80/api/Api_AdSisUs/',
+    BASE_URL_EXT: 'http://10.200.4.164:5000/api/',
+    BASE_URL_LOGIN: 'http://10.200.4.164/'
 
     },
     
