71a11689c15b19e3e4f321892b7c3ee00124421e
abelinoolguin0402 <112125177+abelinoolguin0402@users.noreply.github.com>
2023-04-13 17:53:58 -0600

    validaciones a cuenta bancaria
    validaciones a cuenta bancaria

--- src/app/views/components/DAF/AsigPago.tsx
+++ src/app/views/components/DAF/AsigPago.tsx
@@ -617,7 +617,102 @@ const AsigPago = () => {
                       <Typography color="inherit">Cargar SPEI's</Typography>
                       {"Solo se puede cargar en forma masiva si el Estatus es "}
                       <b>{"'Pendiente de Spei'"}</b>
-                      
+                      <div
+              style={{
+                height: "58vh",
+                width: "100%",
+              }}
+            >
+              <ThemeProvider theme={theme}>
+                <DataGrid
+                  columns={columnsParticipaciones}
+                  rows={data}
+                  density="compact"
+                  rowsPerPageOptions={[10, 25, 50, 100]}
+                  disableSelectionOnClick
+                  disableColumnFilter
+                  disableColumnSelector
+                  disableDensitySelector
+                  getRowHeight={() => "auto"}
+                  getRowClassName={(params) => {
+                    if (params.row.Presupuesto == null) {
+                      return "";
+                    }
+                    return clsx("super-app", {
+                      negative: params.row.Presupuesto !== params.row.total,
+                      positive: params.row.Presupuesto == params.row.total,
+                    });
+                  }}
+                  components={{ Toolbar: GridToolbar }}
+                  sx={{
+                    fontFamily: "Poppins,sans-serif",
+                    fontWeight: "600",
+                    "& .super-app.negative": {
+                      color: "rgb(84, 3, 3)",
+                      backgroundColor: "rgb(196, 40, 40, 0.384)",
+                    },
+                    "& .super-app.positive": {
+                      backgroundColor: "rgb(16, 145, 80, 0.567)",
+                    },
+                  }}
+                  componentsProps={{
+                    toolbar: {
+                      label: "Buscar",
+                      showQuickFilter: true,
+
+                      quickFilterProps: { debounceMs: 500 },
+
+                      csvOptions: {
+                        fileName: "Distribucion",
+                        utf8WithBom: true,
+                      },
+                    },
+                  }}
+                  checkboxSelection={checkboxSelection}
+                  onSelectionModelChange={(newSelectionModel: any) => {
+                    setSelectionModel(newSelectionModel);
+                  }}
+                  selectionModel={selectionModel}
+                  localeText={{
+                    noRowsLabel: "No se ha encontrado datos.",
+                    noResultsOverlayLabel:
+                      "No se ha encontrado ningún resultado",
+                    toolbarColumns: "Columnas",
+                    toolbarExport: "Exportar",
+                    toolbarColumnsLabel: "Seleccionar columnas",
+                    toolbarFilters: "Filtros",
+                    toolbarFiltersLabel: "Ver filtros",
+                    toolbarFiltersTooltipHide: "Quitar filtros",
+                    toolbarFiltersTooltipShow: "Ver filtros",
+                    toolbarQuickFilterPlaceholder: "Buscar",
+                    toolbarExportCSV: "Descargar como CSV",
+                    toolbarExportPrint: "Imprimir",
+                    checkboxSelectionSelectRow: "Filas seleccionadas",
+                    checkboxSelectionSelectAllRows:
+                      "Seleccionar todas las filas",
+                    errorOverlayDefaultLabel: "Ha ocurrido un error.",
+                    footerRowSelected: (count) =>
+                      count > 1
+                        ? `${count.toLocaleString()} filas seleccionadas`
+                        : `${count.toLocaleString()} fila seleccionada`,
+                    footerTotalRows: "Filas Totales:",
+                    columnMenuLabel: "Menú",
+                    columnMenuShowColumns: "Mostrar columnas",
+                    columnMenuFilter: "Filtro",
+                    columnMenuHideColumn: "Ocultar",
+                    columnMenuUnsort: "Desordenar",
+                    columnMenuSortAsc: "Ordenar ASC",
+                    columnMenuSortDesc: "Ordenar DESC",
+                    columnHeaderFiltersTooltipActive: (count) =>
+                      count > 1
+                        ? `${count} filtros activos`
+                        : `${count} filtro activo`,
+                    columnHeaderFiltersLabel: "Mostrar filtros",
+                    columnHeaderSortIconLabel: "Ordenar",
+                  }}
+                />
+              </ThemeProvider>
+            </div>
                     </React.Fragment>
                   }
                 >
--- src/app/views/components/componentes/CustomizedTooltips.tsx
+++ src/app/views/components/componentes/CustomizedTooltips.tsx
@@ -19,9 +19,10 @@ export const TooltipPersonalizado = styled(({ className, ...props }: TooltipProp
   [`& .${tooltipClasses.tooltip}`]: {
     backgroundColor: '#f5f5f9',
     color: 'rgba(0, 0, 0, 0.87)',
-    maxWidth: 500,
+    maxWidth: 1000,
     fontSize: theme.typography.pxToRem(12),
     border: '1px solid #dadde9',
+    
   },
 }));
 
