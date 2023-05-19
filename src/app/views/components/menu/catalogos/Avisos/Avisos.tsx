ccf4378c4fb8565e754859585773a0afbf727091
abelinoolguin0402 <112125177+abelinoolguin0402@users.noreply.github.com>
2023-05-19 17:54:59 -0600

    Update Avisos.tsx

--- src/app/views/components/menu/catalogos/Avisos/Avisos.tsx
+++ src/app/views/components/menu/catalogos/Avisos/Avisos.tsx
@@ -57,7 +57,7 @@ export const Avisos = () => {
     { field: "FechaFin",        headerName: "Expiración",     description: "Expiración",      width: 200 },
     { field: "Nombre",          headerName: "Nombre",         description: "Nombre",          width: 250 },
     { field: "Descripcion",     headerName: "Descripción",    description: "Descripción",     width: 500, },
-    { field: "NombreDocumento", headerName: "NombreDocumento",description: "NombreDocumento", hide: true, width: 150, },
+    { field: "NombreDocumento", headerName: "Nombre Documento",description: "Nombre Documento", hide: true, width: 150, },
   ];
 
 
