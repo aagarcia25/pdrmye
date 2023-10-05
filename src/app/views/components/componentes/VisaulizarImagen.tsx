e3833505a56812bf9427118bc4d6bc98cf9f0dd2
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-10-05 12:17:32 -0600

    ajustes

--- src/app/views/components/componentes/VisaulizarImagen.tsx
+++ src/app/views/components/componentes/VisaulizarImagen.tsx
@@ -1,51 +1,39 @@
-import { Grid } from '@mui/material';
-import { useEffect, useState } from 'react';
-import { AuthService } from '../../../services/AuthService';
-import { Blanco } from '../../../styles/imagen';
-import SliderProgress from '../SliderProgress';
-import SliderProgressLoading from '../SliderProgressLoading';
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import { useEffect, useState } from "react";
+import { AuthService } from "../../../services/AuthService";
 interface Props {
-    ubicacion: string;
-    name: string;
+  ubicacion: string;
+  name: string;
 }
 
-export const VisaulizarImagen = (
-    {
-        ubicacion,
-        name
-    }: Props) => {
-    const [TIPO, setTIPO] = useState<string>("");
-    const [FILE, setFILE] = useState<string>("");
+export const VisaulizarImagen = ({ ubicacion, name }: Props) => {
+  const [TIPO, setTIPO] = useState<string>("");
+  const [FILE, setFILE] = useState<string>("");
 
-    const GetImage = () => {
-        AuthService.GetImagen(ubicacion, name).then((res) => {
-            if (res.RESPONSE.SUCCESS) {
-                setTIPO(res.RESPONSE.RESPONSE.TIPO);
-                setFILE(res.RESPONSE.RESPONSE.FILE);
-            }
-        });
-    };
+  const GetImage = () => {
+    /* AuthService.GetImagen(ubicacion, name).then((res) => {
+      if (res.RESPONSE.SUCCESS) {
+        setTIPO(res.RESPONSE.RESPONSE.TIPO);
+        setFILE(res.RESPONSE.RESPONSE.FILE);
+      }
+    });*/
+  };
 
-    useEffect(() => {
-        GetImage()
+  useEffect(() => {
+    GetImage();
+  }, [name]);
 
-    }, [name]);
-
-
-    return (
-        <div className='containerCenterGenerico' >
-            {FILE ?
-            // <SliderProgressLoading />
-                <img  style={{ objectFit: "scale-down", width: "100%", height: "100%", }}
-                    src={"data:" + TIPO + ";base64," + FILE} />
-                :
-                <SliderProgressLoading />
-                    
-            }
-        </div>
-
-    )
-}
-
-
-
+  return (
+    <div className="containerCenterGenerico">
+      {FILE ? (
+        // <SliderProgressLoading />
+        <img
+          style={{ objectFit: "scale-down", width: "100%", height: "100%" }}
+          src={"data:" + TIPO + ";base64," + FILE}
+        />
+      ) : (
+        <AddPhotoAlternateIcon sx={{ width: "100%", height: "100%" }} />
+      )}
+    </div>
+  );
+};
