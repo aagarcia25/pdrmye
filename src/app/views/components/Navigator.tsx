8a2de950c08969a42b3ed33743c6b96f860c35dc
noreply-cecapmex <baguirre@cecapmex.com>
2024-11-12 16:06:13 -0600

    [PDRMYE][BRYAN]->Diseño de menu

--- src/app/views/components/Navigator.tsx
+++ src/app/views/components/Navigator.tsx
@@ -46,14 +46,25 @@ export default function Navigator(props: DrawerProps, logoFijo: any) {
           container
           position="sticky"
           alignContent="center"
-          sx={{ bgcolor: "rgb(255, 255, 255)", width: "100%", display: 'flex', justifyContent: 'center' }}
+          sx={{ bgcolor: "rgb(255, 255, 255)", width: "100%",  justifyContent: 'center' }}
         >
-          <Grid item xs={10.5} sx={{ width: "auto", height: "5%" }}>
-            <img src={Logo} style={{ width: "100%" }} onClick={() => reedireccion("/")} />
+           <Grid item xs={10.5}sx={{ width: "auto" }}>
+          <img
+            src={Logo}
+            style={{ width: "100%" }}
+            onClick={() => reedireccion("/")}
+          />
+        </Grid>
+        <Grid
+          item
+          sx={{ width: "auto", textAlign: "center", paddingLeft: "3%" }}
+        >
+          <Typography variant="h6" sx={{ fontWeight: "550px" }}>
+           
+            DISTRIBUCIÓN DE RECURSOS
+          </Typography>
           </Grid>
-          <Grid item sx={{ width: "auto", textAlign: "center", paddingLeft: "3%" }}>
-            <Typography variant="h6" sx={{ fontWeight: "550" }}>DISTRIBUCIÓN DE RECURSOS</Typography>
-          </Grid>
+          
         </Grid>
         <Divider sx={{ m: '1vh' }} />
         <Box sx={{ overflow: "auto", scrollbarWidth: "thin", "&::-webkit-scrollbar": { width: "0.4em" }, "&::-webkit-scrollbar-track": { background: "#f1f1f1" }, "&::-webkit-scrollbar-thumb": { backgroundColor: "#888" }, "&::-webkit-scrollbar-thumb:hover": { background: "#555" } }}>
