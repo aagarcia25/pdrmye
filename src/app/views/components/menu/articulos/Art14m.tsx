f9f34e5be2cf28b72af0a3e08ad8aa68f054dce0
PedroPardoG <112125266+PedroPardoG@users.noreply.github.com>
2025-02-17 14:21:07 -0600

    correccion FI

--- src/app/views/components/menu/articulos/Art14m.tsx
+++ src/app/views/components/menu/articulos/Art14m.tsx
@@ -37,7 +37,7 @@ const Art14m = ({
 
   const handleMontos = (v: any) => {
     setmontoaniopasado(v);
-    setMonto(importeDistri.reduce((a, b) => a + b, 0) - v);
+    setMonto(importeDistri.reduce((a, b) => a + b, 0) - (v/12));
   };
 
   const handleclose = () => {
